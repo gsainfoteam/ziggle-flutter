d0fad29199fc5475099e31fb4b4353ad533299fb
2paperstar <lee@paperst.ar>
2023-07-04 22:53:23 +0900

    chore: change idp url

--- lib/app/core/values/strings.dart
+++ lib/app/core/values/strings.dart
@@ -1,8 +1,7 @@
 const apiBaseUrl = 'https://api.stg.ziggle.gistory.me';
 const _idpClientId = 'ziggle2023';
 const idpRedirectScheme = 'ziggle-idp-login-redirect';
-// const idpUrl = 'https://gistory-idp-fe.pages.dev/'
-const idpUrl = 'https://infoteam-idp.web.app/'
+const idpUrl = 'https://gistory-idp-fe.pages.dev/'
     '?client_id=$_idpClientId'
     '&redirect_uri=ziggle'
     '&scheme=$idpRedirectScheme'
