1a0c5485dd04c02b0792ad74a8a375ebef696461
2paperstar <lee@paperst.ar>
2023-07-13 18:17:25 +0900

    feat: android heads-up notification

--- lib/app/data/provider/fcm.dart
+++ lib/app/data/provider/fcm.dart
@@ -1,11 +1,19 @@
 import 'dart:async';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+const _androidNotificationChannel = AndroidNotificationChannel(
+  'ziggle_notification_channel',
+  '지글 알림',
+  importance: Importance.max,
+);
 
 class FcmProvider {
   String? _token;
   final _controller = StreamController<String?>.broadcast();
   final _completer = Completer<void>();
+  final _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
   FcmProvider() {
     _controller.stream.listen((event) {
@@ -30,6 +38,34 @@ class FcmProvider {
       badge: true,
       sound: true,
     );
+    _flutterLocalNotificationsPlugin.initialize(
+      const InitializationSettings(
+        android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+      ),
+    );
+    await _flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(_androidNotificationChannel);
+    FirebaseMessaging.onMessage.listen(_androidMessageListener);
+  }
+
+  void _androidMessageListener(RemoteMessage rm) {
+    final notification = rm.notification;
+
+    if (notification != null && notification.android != null) {
+      _flutterLocalNotificationsPlugin.show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            _androidNotificationChannel.id,
+            _androidNotificationChannel.name,
+          ),
+        ),
+      );
+    }
   }
 
   Stream<String?> getFcmToken() async* {
