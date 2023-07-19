c87e5c669dc14a62529015d877d7525f5ddbe473
2paperstar <lee@paperst.ar>
2023-07-20 07:41:41 +0900

    feat: add new article types

--- lib/app/data/enums/article_type.dart
+++ lib/app/data/enums/article_type.dart
@@ -1,13 +1,20 @@
 enum ArticleType {
+  deadline('⭐️', '기한 임박', 1),
+  hot('🔥', '요즘 끓는 공지', 1),
   recruit('🎯', '모집', 1),
   event('🎈', '행사', 1),
   general('🔔', '일반', 1),
-  academic('📰', '학사', 1);
+  academic('📰', '학사공지', 1, shortTitle: '학사');
 
   final String emoji;
   final String title;
+  final String shortTitle;
   final int id;
-  String get label => '$emoji $title';
+  String get label => '$emoji $shortTitle';
 
-  const ArticleType(this.emoji, this.title, this.id);
+  static const searchables = [recruit, event, general, academic];
+  static const writables = [recruit, event, general];
+
+  const ArticleType(this.emoji, this.title, this.id, {String? shortTitle})
+      : shortTitle = shortTitle ?? title;
 }
