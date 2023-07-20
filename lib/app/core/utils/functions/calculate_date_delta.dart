9768e3c4fdc143ea77254339ae262f866f22f5e1
2paperstar <lee@paperst.ar>
2023-07-20 18:02:45 +0900

    feat: deadline section page

--- lib/app/core/utils/functions/calculate_date_delta.dart
+++ lib/app/core/utils/functions/calculate_date_delta.dart
@@ -1,6 +1,5 @@
+import 'package:ziggle/app/core/utils/extension/date_align.dart';
+
 /// b - a
-int calculateDateDelta(DateTime a, DateTime b) => b
-    .copyWith(hour: 0, minute: 0, second: 0, millisecond: 0, microsecond: 0)
-    .difference(a.copyWith(
-        hour: 0, minute: 0, second: 0, millisecond: 0, microsecond: 0))
-    .inDays;
+int calculateDateDelta(DateTime a, DateTime b) =>
+    b.aligned.difference(a.aligned).inDays;
--- lib/app/modules/article_section/page.dart
+++ lib/app/modules/article_section/page.dart
@@ -1,6 +1,11 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ziggle/app/core/theme/text.dart';
+import 'package:ziggle/app/core/utils/extension/date_align.dart';
+import 'package:ziggle/app/core/utils/functions/calculate_date_delta.dart';
+import 'package:ziggle/app/core/values/colors.dart';
+import 'package:ziggle/app/data/enums/article_type.dart';
 import 'package:ziggle/app/global_widgets/article_card.dart';
 import 'package:ziggle/app/modules/article_section/controller.dart';
 
@@ -33,24 +38,70 @@ class ArticleSectionPage extends GetView<ArticleSectionController> {
                 ? const SizedBox.shrink().sliverBox
                 : SliverPadding(
                     padding: const EdgeInsets.symmetric(horizontal: 20),
-                    sliver: SliverList.builder(
-                      itemCount: controller.articles.value!.length,
-                      itemBuilder: (context, index) {
-                        return SizedBox(
-                          height: 170,
-                          child: ArticleCard(
-                            article: controller.articles.value![index],
-                            direction: Axis.horizontal,
-                            onTap: () => controller.goToDetail(
-                                controller.articles.value![index].id),
-                          ),
-                        ).marginOnly(bottom: 18);
-                      },
-                    ),
+                    sliver: controller.type == ArticleType.deadline
+                        ? Obx(_buildDeadlineList)
+                        : Obx(_buildSimpleList),
                   ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildDeadlineList() {
+    final articles = groupBy(
+      controller.articles.value!,
+      (article) => article.deadline!.aligned,
+    ).entries.toList();
+    return SliverList.builder(
+      itemCount: articles.length,
+      itemBuilder: (context, index) {
+        return Column(
+          children: [
+            Text(
+              '${calculateDateDelta(DateTime.now(), articles[index].key)}일 남음',
+              style: TextStyles.articleCardTitleStyle,
+            ),
+            const SizedBox(height: 12),
+            Container(width: 80, height: 1, color: Palette.black),
+            ListView(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              children: articles[index]
+                  .value
+                  .map(
+                    (article) => SizedBox(
+                      height: 170,
+                      child: ArticleCard(
+                        article: article,
+                        direction: Axis.horizontal,
+                        onTap: () => controller.goToDetail(article.id),
+                      ),
+                    ).marginOnly(bottom: 18),
+                  )
+                  .toList(),
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildSimpleList() {
+    final articles = controller.articles.value!;
+    return SliverList.builder(
+      itemCount: articles.length,
+      itemBuilder: (context, index) {
+        return SizedBox(
+          height: 170,
+          child: ArticleCard(
+            article: articles[index],
+            direction: Axis.horizontal,
+            onTap: () => controller.goToDetail(articles[index].id),
+          ),
+        ).marginOnly(bottom: 18);
+      },
     );
   }
 }
