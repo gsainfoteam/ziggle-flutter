4da0d1fc44f176fe62ef08df45771801a527e66e
2paperstar <lee@paperst.ar>
2023-07-21 00:40:50 +0900

    feat: search query

--- lib/app/modules/search/repository.dart
+++ lib/app/modules/search/repository.dart
@@ -1,3 +1,4 @@
+import 'package:ziggle/app/data/enums/article_type.dart';
 import 'package:ziggle/app/data/model/article_summary_response.dart';
 import 'package:ziggle/app/data/provider/api.dart';
 
@@ -6,7 +7,14 @@ class SearchRepository {
 
   SearchRepository(this._provider);
 
-  Future<List<ArticleSummaryResponse>> search(String query) {
-    return _provider.getNotices();
+  Future<List<ArticleSummaryResponse>> search(
+    String query, [
+    ArticleType? type,
+  ]) {
+    return _provider.getNotices(
+      limit: 10,
+      search: query,
+      tags: type == null ? null : [type.name],
+    );
   }
 }
