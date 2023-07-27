ebe0eb7d293bf142f0a32171813349b13821f253
2paperstar <lee@paperst.ar>
2023-07-27 16:36:47 +0900

    chore: remove user email field from user info

--- lib/app/data/model/user_info_response.g.dart
+++ lib/app/data/model/user_info_response.g.dart
@@ -11,7 +11,6 @@ _$_UserInfoResponse _$$_UserInfoResponseFromJson(Map<String, dynamic> json) =>
       uuid: json['user_uuid'] as String,
       email: json['user_email_id'] as String,
       name: json['user_name'] as String,
-      phoneNumber: json['user_phone_number'] as String,
       studentId: json['student_id'] as String,
     );
 
@@ -20,6 +19,5 @@ Map<String, dynamic> _$$_UserInfoResponseToJson(_$_UserInfoResponse instance) =>
       'user_uuid': instance.uuid,
       'user_email_id': instance.email,
       'user_name': instance.name,
-      'user_phone_number': instance.phoneNumber,
       'student_id': instance.studentId,
     };
