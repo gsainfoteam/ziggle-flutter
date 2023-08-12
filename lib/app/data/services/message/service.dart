b448d9191b1c700b0a9e05c44a048d22cfb3f1cf
2paperstar <lee@paperst.ar>
2023-08-12 13:35:57 +0900

    fix: if path same, back first

--- lib/app/data/services/message/service.dart
+++ lib/app/data/services/message/service.dart
@@ -8,7 +8,11 @@ class MessageService {
 
   MessageService(this._fcmProvider, this._userService) {
     _userService.getUserInfo().first.then((_) {
-      _fcmProvider.getLink().listen((event) {
+      _fcmProvider.getLink().listen((event) async {
+        if (Uri.parse(Get.currentRoute).path == Uri.parse(event).path) {
+          Get.back();
+          await Future.delayed(const Duration(milliseconds: 500));
+        }
         Get.toNamed(event);
       });
     });
