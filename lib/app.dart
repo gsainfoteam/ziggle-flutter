1e049fe1a9c1fa9e043841a8e532c0fec8cc7cd7
2paperstar <lee@paperst.ar>
2023-08-26 12:02:21 +0900

    feat: smartlook recording

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_smartlook/flutter_smartlook.dart';
 import 'package:get/get.dart';
 import 'package:upgrader/upgrader.dart';
 import 'package:ziggle/app/core/theme/app.dart';
@@ -11,35 +12,52 @@ import 'package:ziggle/gen/strings.g.dart';
 
 import 'app/routes/pages.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final smartlook = Smartlook.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    smartlook.start();
+    smartlook.preferences
+        .setProjectKey('559177df225fb6be8f57ab026bcd071f18c172cc');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion(
-      value: const SystemUiOverlayStyle(
-        systemNavigationBarColor: Palette.white,
-      ),
-      child: GestureDetector(
-        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-        child: GetMaterialApp(
-          locale: TranslationProvider.of(context).flutterLocale,
-          supportedLocales: AppLocaleUtils.supportedLocales,
-          localizationsDelegates: GlobalMaterialLocalizations.delegates,
-          debugShowCheckedModeBanner: false,
-          initialRoute: AppPages.INITIAL,
-          initialBinding: initialBinding,
-          getPages: AppPages.routes,
-          theme: appTheme,
-          navigatorObservers: [AnalyticsService.observer],
-          builder: (context, child) => UpgradeAlert(
-            upgrader: Upgrader(
-              dialogStyle: UpgradeDialogStyle.cupertino,
-              showIgnore: false,
-              showLater: false,
+    return SmartlookRecordingWidget(
+      child: AnnotatedRegion(
+        value: const SystemUiOverlayStyle(
+          systemNavigationBarColor: Palette.white,
+        ),
+        child: GestureDetector(
+          onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+          child: GetMaterialApp(
+            locale: TranslationProvider.of(context).flutterLocale,
+            supportedLocales: AppLocaleUtils.supportedLocales,
+            localizationsDelegates: GlobalMaterialLocalizations.delegates,
+            debugShowCheckedModeBanner: false,
+            initialRoute: AppPages.INITIAL,
+            initialBinding: initialBinding,
+            getPages: AppPages.routes,
+            theme: appTheme,
+            navigatorObservers: [AnalyticsService.observer],
+            builder: (context, child) => UpgradeAlert(
+              upgrader: Upgrader(
+                dialogStyle: UpgradeDialogStyle.cupertino,
+                showIgnore: false,
+                showLater: false,
+              ),
+              navigatorKey: Get.key,
+              child: child,
             ),
-            navigatorKey: Get.key,
-            child: child,
           ),
         ),
       ),
