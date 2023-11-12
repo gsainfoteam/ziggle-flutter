0c466f50f364ab8c9e7f1e613b0d9042fd771c7a
2paperstar <lee@paperst.ar>
2023-11-12 15:50:37 +0900

    feat: add UserEntity

---
+++ lib/app/modules/auth/domain/entities/user_entity.dart
@@ -0,0 +1,13 @@
+class UserEntity {
+  final int id;
+  final String email;
+  final String name;
+  final String studentId;
+
+  UserEntity({
+    required this.id,
+    required this.email,
+    required this.name,
+    required this.studentId,
+  });
+}
