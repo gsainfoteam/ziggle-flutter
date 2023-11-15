0e04aab516ed0fde1eed18ef254dc1001379e5df
2paperstar <lee@paperst.ar>
2023-11-16 07:11:15 +0900

    chore(deps): remove get, infinite_scroll_pagination

--- lib/binding.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-import 'package:ziggle/app/data/provider/api.dart';
-import 'package:ziggle/app/data/provider/db.dart';
-
-final initialBinding = BindingsBuilder(() {
-  Get.lazyPut(() => ApiProvider(Get.find()), fenix: true);
-  Get.lazyPut(() => DbProvider(), fenix: true);
-});
