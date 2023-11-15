017cb15e1ee233cafcb15cb7e84b067ebfe990d2
2paperstar <lee@paperst.ar>
2023-11-15 18:12:05 +0900

    refactor: don't use obx in button

--- lib/app/common/presentaion/widgets/button.dart
+++ lib/app/common/presentaion/widgets/button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ziggle/app/core/values/palette.dart';
 
-class ZiggleButton extends StatelessWidget {
+class ZiggleButton extends StatefulWidget {
   final Widget? child;
   final void Function()? onTap;
   final BoxDecoration decoration;
@@ -13,8 +13,6 @@ class ZiggleButton extends StatelessWidget {
   final TextStyle? textStyle;
   final EdgeInsets? padding;
   final bool loading;
-  bool get _isTransparent => color == Colors.transparent;
-
   const ZiggleButton({
     super.key,
     this.child,
@@ -35,57 +33,62 @@ class ZiggleButton extends StatelessWidget {
             );
 
   @override
+  State<ZiggleButton> createState() => _ZiggleButtonState();
+}
+
+class _ZiggleButtonState extends State<ZiggleButton> {
+  bool get _isTransparent => widget.color == Colors.transparent;
+  bool _hover = false;
+
+  @override
   Widget build(BuildContext context) {
-    final hover = false.obs;
     return GestureDetector(
-      onTap: loading ? null : onTap,
-      onTapDown: (_) => hover.value = true,
-      onTapCancel: () => hover.value = false,
-      onTapUp: (_) => hover.value = false,
-      child: Obx(
-        () => AnimatedContainer(
-          duration: 100.milliseconds,
-          padding: padding ??
-              EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: _isTransparent ? 0 : 10,
-              ),
-          decoration: decoration.copyWith(
-            color: Color.lerp(
-              color,
-              Palette.black,
-              loading
-                  ? 0.4
-                  : hover.value && onTap != null
-                      ? 0.1
-                      : 0,
+      onTap: widget.loading ? null : widget.onTap,
+      onTapDown: (_) => setState(() => _hover = true),
+      onTapCancel: () => setState(() => _hover = false),
+      onTapUp: (_) => setState(() => _hover = false),
+      child: AnimatedContainer(
+        duration: 100.milliseconds,
+        padding: widget.padding ??
+            EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: _isTransparent ? 0 : 10,
             ),
+        decoration: widget.decoration.copyWith(
+          color: Color.lerp(
+            widget.color,
+            Palette.black,
+            widget.loading
+                ? 0.4
+                : _hover && widget.onTap != null
+                    ? 0.1
+                    : 0,
           ),
-          child: _buildChild(),
         ),
+        child: _buildChild(),
       ),
     );
   }
 
   _buildChild() {
-    if (text == null) {
-      return child ?? const SizedBox.shrink();
+    if (widget.text == null) {
+      return widget.child ?? const SizedBox.shrink();
     }
     return Stack(
       clipBehavior: Clip.none,
       alignment: Alignment.center,
       children: [
         Text(
-          text!,
-          style: textStyle ??
+          widget.text!,
+          style: widget.textStyle ??
               TextStyle(
-                fontSize: fontSize,
-                color: _isTransparent ? Palette.black : textColor,
+                fontSize: widget.fontSize,
+                color: _isTransparent ? Palette.black : widget.textColor,
                 fontWeight:
                     _isTransparent ? FontWeight.normal : FontWeight.bold,
               ),
         ),
-        if (loading)
+        if (widget.loading)
           Positioned.fill(
             child: LayoutBuilder(builder: (context, constraints) {
               return Center(
