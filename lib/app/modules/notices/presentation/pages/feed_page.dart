55a8ad6c4261d38c8133791bb05c1f59378d5752
2paperstar <lee@paperst.ar>
2024-01-13 21:31:55 +0900

    feat: feed page appbar

--- lib/app/modules/notices/presentation/pages/feed_page.dart
+++ lib/app/modules/notices/presentation/pages/feed_page.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:ziggle/gen/assets.gen.dart';
 
 class FeedPage extends StatelessWidget {
   const FeedPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Text('test'),
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            leading: Center(
+              child: Assets.logo.black.image(height: 48),
+            ),
+            actions: [
+              IconButton(
+                icon: Assets.icons.search.image(),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Assets.icons.bell.image(),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Assets.icons.user.image(),
+                onPressed: () {},
+              ),
+            ],
+          ),
         ],
       ),
     );
