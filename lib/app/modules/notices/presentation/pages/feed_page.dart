1e24051a985e16e7ad03edf6e3756bf77d248475
2paperstar <lee@paperst.ar>
2024-01-13 22:45:10 +0900

    feat: feed page refresh indicator

--- lib/app/modules/notices/presentation/pages/feed_page.dart
+++ lib/app/modules/notices/presentation/pages/feed_page.dart
@@ -11,70 +11,80 @@ class FeedPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final mediaQueryPadding = MediaQuery.paddingOf(context);
+    final toolbarHeight = Theme.of(context).appBarTheme.toolbarHeight!;
+    final bottomHeight = toolbarHeight + 8;
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            toolbarHeight: Theme.of(context).appBarTheme.toolbarHeight!,
-            floating: true,
-            leading: Center(
-              child: Assets.logo.black.image(height: 48),
-            ),
-            actions: [
-              IconButton(
-                icon: Assets.icons.search.image(),
-                onPressed: () {},
+      body: RefreshIndicator(
+        edgeOffset: mediaQueryPadding.top + toolbarHeight + bottomHeight,
+        onRefresh: () {
+          HapticFeedback.mediumImpact();
+          return Future.delayed(const Duration(seconds: 1));
+        },
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              toolbarHeight: toolbarHeight,
+              floating: true,
+              leading: Center(
+                child: Assets.logo.black.image(height: toolbarHeight),
               ),
-              IconButton(
-                icon: Assets.icons.editPencil.image(),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Assets.icons.user.image(),
-                onPressed: () {},
-              ),
-            ],
-            bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(48),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 18),
-                  child: Wrap(
-                    spacing: 8,
-                    children: NoticeType.sections
-                        .map(
-                          (e) => ActionChip.elevated(
-                            avatar: e.icon.image(
-                              width: 16,
-                              color: e == NoticeType.all
-                                  ? Palette.background100
-                                  : null,
+              actions: [
+                IconButton(
+                  icon: Assets.icons.search.image(),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Assets.icons.editPencil.image(),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Assets.icons.user.image(),
+                  onPressed: () {},
+                ),
+              ],
+              bottom: PreferredSize(
+                preferredSize: Size.fromHeight(bottomHeight),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 18),
+                    child: Wrap(
+                      spacing: 8,
+                      children: NoticeType.sections
+                          .map(
+                            (e) => ActionChip.elevated(
+                              avatar: e.icon.image(
+                                width: 16,
+                                color: e == NoticeType.all
+                                    ? Palette.background100
+                                    : null,
+                              ),
+                              label: Text(t.notice.type(type: e)),
+                              onPressed: () {
+                                HapticFeedback.lightImpact();
+                              },
+                              labelStyle: TextStyle(
+                                color: e == NoticeType.all
+                                    ? Palette.background100
+                                    : null,
+                              ),
+                              backgroundColor:
+                                  e == NoticeType.all ? Palette.text100 : null,
                             ),
-                            label: Text(t.notice.type(type: e)),
-                            onPressed: () {
-                              HapticFeedback.lightImpact();
-                            },
-                            labelStyle: TextStyle(
-                              color: e == NoticeType.all
-                                  ? Palette.background100
-                                  : null,
-                            ),
-                            backgroundColor:
-                                e == NoticeType.all ? Palette.text100 : null,
-                          ),
-                        )
-                        .toList(),
+                          )
+                          .toList(),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          SliverList.separated(
-            itemBuilder: (context, index) => const NoticeCard(),
-            separatorBuilder: (context, index) => const Divider(),
-          ),
-        ],
+            SliverList.separated(
+              itemBuilder: (context, index) => const NoticeCard(),
+              separatorBuilder: (context, index) => const Divider(),
+            ),
+          ],
+        ),
       ),
     );
   }
