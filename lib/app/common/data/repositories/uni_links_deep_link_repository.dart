f84ff5eaa24e763e1ce4253cc72492b6436e8c18
2paperstar <lee@paperst.ar>
2024-01-13 14:11:54 +0900

    feat: redirect to profile page

--- lib/app/common/data/repositories/uni_links_deep_link_repository.dart
+++ lib/app/common/data/repositories/uni_links_deep_link_repository.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:injectable/injectable.dart';
 import 'package:uni_links/uni_links.dart';
 import 'package:ziggle/app/common/domain/repositories/deep_link_repository.dart';
+import 'package:ziggle/app/core/routes/routes.dart';
 
 @Singleton(as: DeepLinkRepository)
 class UniLinksDeepLinkRepository implements DeepLinkRepository {
@@ -38,8 +39,11 @@ class UniLinksDeepLinkRepository implements DeepLinkRepository {
   Stream<String> getLink() => _getLink().map((link) {
         final uri = Uri.parse(link);
         if (uri.path == '/app') {
-          final redirect = uri.queryParameters['redirect'] ?? '';
-          return '/root${redirect.replaceFirst('notice', 'article')}';
+          final redirect = (uri.queryParameters['redirect'] ?? '')
+              .replaceFirst('/notice', '/article')
+              .replaceFirst('/mypage', '/profile');
+          if (redirect.isNotEmpty) return '/root$redirect';
+          return Paths.home;
         }
         return uri.toString();
       });
