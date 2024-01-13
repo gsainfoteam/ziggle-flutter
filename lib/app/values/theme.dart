55f9e42c064267b7d9ddd47618b2cc27c495accc
2paperstar <lee@paperst.ar>
2024-01-13 22:32:32 +0900

    fix: change scrolled under elevation of app bar

--- lib/app/values/theme.dart
+++ lib/app/values/theme.dart
@@ -5,7 +5,10 @@ import 'package:ziggle/app/values/palette.dart';
 abstract class AppTheme {
   static final theme = ThemeData(
     fontFamily: Pretendard.fontFamily,
-    appBarTheme: const AppBarTheme(toolbarHeight: 40),
+    appBarTheme: const AppBarTheme(
+      toolbarHeight: 40,
+      scrolledUnderElevation: 0,
+    ),
     chipTheme: const ChipThemeData(
       elevation: 0,
       pressElevation: 0,
