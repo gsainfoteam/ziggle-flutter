ab164bc4a5b6341b2551db5114de1780e34f7a2a
2paperstar <lee@paperst.ar>
2024-01-15 11:30:05 +0900

    feat: implement notice repository

---
+++ lib/app/modules/notices/data/repositories/remote_notice_repository.dart
@@ -0,0 +1,22 @@
+import 'package:injectable/injectable.dart';
+import 'package:ziggle/app/modules/notices/data/data_sources/remote/notice_api.dart';
+import 'package:ziggle/app/modules/notices/domain/entities/notice_list_entity.dart';
+import 'package:ziggle/app/modules/notices/domain/repositories/notice_repository.dart';
+
+@Injectable(as: NoticeRepository)
+class RemoteNoticeRepository implements NoticeRepository {
+  final NoticeApi _api;
+
+  RemoteNoticeRepository(this._api);
+
+  @override
+  Future<NoticeListEntity> getNotices(
+      {int? offset, int? limit, String? search, List<String>? tags}) {
+    return _api.getNotices(
+      offset: offset,
+      limit: limit,
+      search: search,
+      tags: tags,
+    );
+  }
+}
