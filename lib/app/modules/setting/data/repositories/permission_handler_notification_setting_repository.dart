508f75e559fdb74416bb07cfd160e732e64858bd
2paperstar <lee@paperst.ar>
2024-01-23 21:46:03 +0900

    feat: use permission handler for check notification permission status

--- lib/app/modules/setting/data/repositories/permission_handler_notification_setting_repository.dart
+++ lib/app/modules/setting/data/repositories/permission_handler_notification_setting_repository.dart
@@ -1,19 +1,31 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:injectable/injectable.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '../../domain/repositories/notification_setting_repository.dart';
 
 @Injectable(as: NotificationSettingRepository)
-class FcmNotificationSettingRepository
+class PermissionHandlerNotificationSettingRepository
     implements NotificationSettingRepository {
   @override
   Future<bool> isNotificationEnabled() async {
-    final setting = await FirebaseMessaging.instance.getNotificationSettings();
-    return setting.authorizationStatus == AuthorizationStatus.authorized;
+    return await Permission.notification.isGranted;
+  }
+
+  Future<bool> _needsOpenSetting() async {
+    final status = await Permission.notification.status;
+    return status.isDenied ||
+        status.isPermanentlyDenied ||
+        status.isProvisional;
   }
 
   @override
   Future<bool> enableNotification() async {
+    final needsOpenSetting = await _needsOpenSetting();
+    if (needsOpenSetting) {
+      await openAppSettings();
+      return false;
+    }
     final result = await FirebaseMessaging.instance.requestPermission(
       announcement: true,
       provisional: true,
