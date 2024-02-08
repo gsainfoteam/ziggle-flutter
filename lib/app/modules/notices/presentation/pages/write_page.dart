b5f41b69f95f64b65faafbe86aab85acc1af6720
2paperstar <lee@paperst.ar>
2024-02-08 15:41:50 +0900

    fix: remove empty tag when call onChange handler

--- lib/app/modules/notices/presentation/pages/write_page.dart
+++ lib/app/modules/notices/presentation/pages/write_page.dart
@@ -531,6 +531,7 @@ class _TagState extends State<_Tag> {
         .split(' ')
         .where((v) => v.startsWith('#'))
         .map((e) => e.substring(1).trim())
+        .where((element) => element.isNotEmpty)
         .toList());
     // if newly focused, add a hash
     if (_focus.hasFocus && !_hasFocus) {
