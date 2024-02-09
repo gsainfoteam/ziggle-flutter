3262e1f76191ded719457f8cd8bed5c6763c5014
2paperstar <lee@paperst.ar>
2024-02-09 21:05:58 +0900

    feat: create fcm repository

---
+++ lib/app/modules/core/data/repositories/fcm_repository.dart
@@ -0,0 +1,17 @@
+import 'package:injectable/injectable.dart';
+
+import '../../domain/repositories/link_repository.dart';
+import '../../domain/repositories/push_message_repository.dart';
+
+@Singleton(as: PushMessageRepository)
+class FcmRepository implements PushMessageRepository, LinkRepository {
+  @override
+  Future<String> getToken() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<String> getLinkStream() {
+    throw UnimplementedError();
+  }
+}
---
+++ lib/app/modules/core/domain/repositories/push_message_repository.dart
@@ -0,0 +1,3 @@
+abstract class PushMessageRepository {
+  Future<String> getToken();
+}
