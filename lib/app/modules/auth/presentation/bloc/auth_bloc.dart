54dd9ffb18d5936d9acc471216029c567a520249
2paperstar <lee@paperst.ar>
2024-02-09 19:01:06 +0900

    feat: auth bloc analytics

--- lib/app/modules/auth/presentation/bloc/auth_bloc.dart
+++ lib/app/modules/auth/presentation/bloc/auth_bloc.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
+import 'package:ziggle/app/modules/core/domain/repositories/analytics_repository.dart';
 
 import '../../domain/entities/user_entity.dart';
 import '../../domain/repositories/auth_repository.dart';
@@ -13,9 +14,13 @@ part 'auth_bloc.freezed.dart';
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final OAuthRepository _oAuthRepository;
   final AuthRepository _authRepository;
+  final AnalyticsRepository _analyticsRepository;
 
-  AuthBloc(this._oAuthRepository, this._authRepository)
-      : super(const _Initial()) {
+  AuthBloc(
+    this._oAuthRepository,
+    this._authRepository,
+    this._analyticsRepository,
+  ) : super(const _Initial()) {
     on<_Load>((event, emit) async {
       emit(const _Loading());
       await emit.forEach(
@@ -25,27 +30,35 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       );
     });
     on<_Login>((event, emit) async {
+      _analyticsRepository.logTryLogin();
       emit(const _Loading());
       try {
         final data = await _oAuthRepository.getAuthorizationCode();
         final user = await _authRepository.login(data.authCode);
         emit(_Authenticated(user));
+        _analyticsRepository.logLogin();
       } catch (e) {
         emit(_Error(e.toString()));
+        _analyticsRepository.logLoginCancel(e.toString());
       }
     });
     on<_Logout>((event, emit) async {
       emit(const _Loading());
       await _oAuthRepository.setRecentLogout();
       await _authRepository.logout();
+      _authRepository.logout();
       emit(const _Guest());
     });
   }
 
-  static UserEntity? userOrNull(BuildContext context) => context
-      .read<AuthBloc>()
-      .state
-      .maybeMap(authenticated: (s) => s.user, orElse: () => null);
+  @override
+  void onChange(Change<AuthState> change) {
+    super.onChange(change);
+    _analyticsRepository.logChangeUser(change.nextState.userOrNull);
+  }
+
+  static UserEntity? userOrNull(BuildContext context) =>
+      context.read<AuthBloc>().state.userOrNull;
 }
 
 @freezed
@@ -67,6 +80,8 @@ class AuthState with _$AuthState {
 
   bool get isLoading => this is _Loading;
   UserEntity get user => (this as _Authenticated).user;
+  UserEntity? get userOrNull =>
+      maybeMap(authenticated: (s) => s.user, orElse: () => null);
   bool get hasUser => this is _Authenticated;
   bool get hasError => this is _Error;
   String get message => (this as _Error).message;
