d9da8e1cb258efaeb1ee5d0134e046ebb68fc9ce
2paperstar <lee@paperst.ar>
2024-02-10 07:31:27 +0900

    fix: use versioned API
    fix #204

--- lib/app/modules/core/domain/enums/api_channel.dart
+++ lib/app/modules/core/domain/enums/api_channel.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/foundation.dart';
 
 enum ApiChannel {
-  staging('https://api.stg.ziggle.gistory.me', 'https://stg.idp.gistory.me'),
-  production('https://api.ziggle.gistory.me', 'https://idp.gistory.me');
+  staging('https://api.stg.ziggle.gistory.me/v1', 'https://stg.idp.gistory.me'),
+  production('https://api.ziggle.gistory.me/v1', 'https://idp.gistory.me');
 
   final String baseUrl;
   final String idpBaseUrl;
