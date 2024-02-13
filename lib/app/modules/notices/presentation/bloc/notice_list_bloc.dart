2219465db2dca2c4e5e6c8231eb7f49c80669cea
2paperstar <lee@paperst.ar>
2024-02-13 23:44:23 +0900

    fix: remove try-catch in notice list bloc

--- lib/app/modules/notices/presentation/bloc/notice_list_bloc.dart
+++ lib/app/modules/notices/presentation/bloc/notice_list_bloc.dart
@@ -27,16 +27,12 @@ class NoticeListBloc extends Bloc<NoticeListEvent, NoticeListState> {
     on<_Droppable>((event, emit) async {
       if (event is _Load) {
         emit(_Loading(type: event.type));
-        try {
-          final data = await _repository.getNotices(
-            type: event.type,
-            search: event.query,
-          );
-          _query = event.query;
-          emit(_Loaded(total: data.total, list: data.list, type: event.type));
-        } catch (_) {
-          emit(_Loaded(total: 0, list: [], type: event.type));
-        }
+        final data = await _repository.getNotices(
+          type: event.type,
+          search: event.query,
+        );
+        _query = event.query;
+        emit(_Loaded(total: data.total, list: data.list, type: event.type));
       } else if (event is _Reset) {
         _query = null;
         emit(_Initial(type: state.type));
