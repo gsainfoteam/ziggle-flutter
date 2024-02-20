c8a3490149bb5e2f5ba52f87f3525c89e83f3ee4
2paperstar <lee@paperst.ar>
2024-02-21 02:44:19 +0900

    feat: confetti easter egg

--- lib/app/modules/setting/presentation/pages/about_page.dart
+++ lib/app/modules/setting/presentation/pages/about_page.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher_string.dart';
@@ -7,8 +10,22 @@ import 'package:ziggle/app/router/routes.dart';
 import 'package:ziggle/app/values/strings.dart';
 import 'package:ziggle/gen/strings.g.dart';
 
-class AboutPage extends StatelessWidget {
+class AboutPage extends StatefulWidget {
   const AboutPage({super.key});
+
+  @override
+  State<AboutPage> createState() => _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage> {
+  final _controller =
+      ConfettiController(duration: const Duration(microseconds: 1));
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,59 +33,80 @@ class AboutPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(t.setting.about),
       ),
-      body: SingleChildScrollView(
-        child: ListBody(
-          children: [
-            ListTile(
-              title: Text(t.setting.feedback.label),
-              onTap: () {
-                sl<AnalyticsRepository>().logOpenFeedback();
-                launchUrlString(Strings.heyDeveloperUrl);
-              },
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ConfettiWidget(
+              confettiController: _controller,
+              blastDirection: -pi / 2,
+              numberOfParticles: 20,
+              maxBlastForce: 100,
+              emissionFrequency: 1,
+              colors: const [
+                Colors.green,
+                Colors.blue,
+                Colors.pink,
+                Colors.orange,
+                Colors.purple
+              ], // manually specify the colors to be used
             ),
-            ListTile(
-              title: Text(t.setting.termsOfService),
-              onTap: () {
-                sl<AnalyticsRepository>().logOpenTermsOfService();
-                launchUrlString(
-                  Strings.termsOfServiceUrl,
-                  mode: LaunchMode.inAppBrowserView,
-                );
-              },
-            ),
-            ListTile(
-              title: Text(t.setting.privacyPolicy),
-              onTap: () {
-                sl<AnalyticsRepository>().logOpenPrivacyPolicy();
-                launchUrlString(
-                  Strings.privacyPolicyUrl,
-                  mode: LaunchMode.inAppBrowserView,
-                );
-              },
-            ),
-            ListTile(
-              title: Text(t.setting.openSourceLicenses),
-              onTap: () => const PackagesRoute().push(context),
-            ),
-            ListTile(
-              title: Text(t.setting.version),
-              leadingAndTrailingTextStyle: const TextStyle(
-                fontSize: 16,
-                color: Colors.black54,
-              ),
-              trailing: FutureBuilder(
-                future: PackageInfo.fromPlatform(),
-                builder: (_, snapshot) => Text(
-                  snapshot.data?.version ?? '',
+          ),
+          SingleChildScrollView(
+            child: ListBody(
+              children: [
+                ListTile(
+                  title: Text(t.setting.feedback.label),
+                  onTap: () {
+                    sl<AnalyticsRepository>().logOpenFeedback();
+                    launchUrlString(Strings.heyDeveloperUrl);
+                  },
                 ),
-              ),
+                ListTile(
+                  title: Text(t.setting.termsOfService),
+                  onTap: () {
+                    sl<AnalyticsRepository>().logOpenTermsOfService();
+                    launchUrlString(
+                      Strings.termsOfServiceUrl,
+                      mode: LaunchMode.inAppBrowserView,
+                    );
+                  },
+                ),
+                ListTile(
+                  title: Text(t.setting.privacyPolicy),
+                  onTap: () {
+                    sl<AnalyticsRepository>().logOpenPrivacyPolicy();
+                    launchUrlString(
+                      Strings.privacyPolicyUrl,
+                      mode: LaunchMode.inAppBrowserView,
+                    );
+                  },
+                ),
+                ListTile(
+                  title: Text(t.setting.openSourceLicenses),
+                  onTap: () => const PackagesRoute().push(context),
+                ),
+                ListTile(
+                  title: Text(t.setting.version),
+                  leadingAndTrailingTextStyle: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.black54,
+                  ),
+                  trailing: FutureBuilder(
+                    future: PackageInfo.fromPlatform(),
+                    builder: (_, snapshot) => Text(
+                      snapshot.data?.version ?? '',
+                    ),
+                  ),
+                ),
+                ListTile(
+                  title: Text(t.setting.infoteam),
+                  onTap: () => _controller.play(),
+                ),
+              ],
             ),
-            ListTile(
-              title: Text(t.setting.infoteam),
-              onTap: () {},
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
