88df0c4494cd0ede42b17295a2e01aa660ee77dd
2paperstar <lee@paperst.ar>
2024-05-23 01:49:33 +0900

    fix: change user field

--- lib/app/modules/auth/data/data_source/models/user_model.dart
+++ lib/app/modules/auth/data/data_source/models/user_model.dart
@@ -9,11 +9,12 @@ part 'user_model.g.dart';
 
 @freezed
 class UserModel with _$UserModel implements UserEntity {
+  @JsonSerializable(fieldRename: FieldRename.snake)
   const factory UserModel({
-    @JsonKey(name: 'email') required String email,
-    @JsonKey(name: 'name') required String name,
-    @JsonKey(name: 'studentNumber') required String studentId,
-    @JsonKey(name: 'uuid') required String uuid,
+    required String email,
+    required String name,
+    required String studentId,
+    required String uuid,
   }) = _UserModel;
 
   factory UserModel.fromJson(Map<String, dynamic> json) =>
