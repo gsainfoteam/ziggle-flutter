974702174965d0b904ab6118982afe3f5e0e4262
2paperstar <lee@paperst.ar>
2024-05-30 05:51:58 +0900

    fix: use production idp only

--- lib/app/modules/core/domain/repositories/api_channel_repository.dart
+++ lib/app/modules/core/domain/repositories/api_channel_repository.dart
@@ -3,8 +3,6 @@ import '../enums/api_channel.dart';
 abstract class ApiChannelRepository {
   void dispose();
   String get baseUrl;
-  String get idpUrl;
-  String get idpClientId;
   Stream<ApiChannel> get channel;
   void setChannel(ApiChannel channel);
   ApiChannel toggleChannel();
