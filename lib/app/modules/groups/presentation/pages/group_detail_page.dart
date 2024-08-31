08c37ddf4406b48bf7747704d549ea2e6f45b50e
dawnfire05 <dawnfire05@naver.com>
2024-08-31 20:26:28 +0900

    chore: update slang

--- lib/app/modules/groups/presentation/pages/group_detail_page.dart
+++ lib/app/modules/groups/presentation/pages/group_detail_page.dart
@@ -12,7 +12,7 @@ class GroupDetailPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Palette.background100,
         leadingWidth: Theme.of(context).appBarTheme.toolbarHeight,
-        title: Text(t.group.title),
+        title: Text(t.group.detail.title),
       ),
       body: DefaultTabController(
         length: 3,
@@ -28,13 +28,13 @@ class GroupDetailPage extends StatelessWidget {
               SliverPersistentHeader(
                 pinned: true,
                 delegate: MyTabBarDelegate(
-                  const TabBar(
-                    labelStyle: TextStyle(
+                  TabBar(
+                    labelStyle: const TextStyle(
                       fontSize: 16,
                       color: Palette.primary100,
                       fontWeight: FontWeight.w500,
                     ),
-                    indicator: BoxDecoration(
+                    indicator: const BoxDecoration(
                       border: Border(
                         bottom: BorderSide(
                           color: Palette.primary100,
@@ -46,9 +46,9 @@ class GroupDetailPage extends StatelessWidget {
                     dividerColor: Palette.text400,
                     dividerHeight: 3,
                     tabs: <Tab>[
-                      Tab(text: '소개'),
-                      Tab(text: '공지'),
-                      Tab(text: '멤버'),
+                      Tab(text: t.group.detail.introduction),
+                      Tab(text: t.group.detail.notice),
+                      Tab(text: t.group.detail.member),
                     ],
                   ),
                 ),
@@ -61,6 +61,61 @@ class GroupDetailPage extends StatelessWidget {
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
                           Text('소개 내용입니다.소개 내용입니다.'),
                           Text('소개 내용입니다.소개 내용입니다.'),
                           Text('소개 내용입니다.소개 내용입니다.'),
@@ -113,85 +168,88 @@ class GroupInfoHeaderDelegate extends SliverPersistentHeaderDelegate {
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Container(
-              height: 90,
-              width: 90,
-              decoration: const BoxDecoration(
-                color: Colors.orange,
-                shape: BoxShape.circle,
-              ),
-            ),
-            const SizedBox(width: 15),
-            const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      '인포팀',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(width: 5),
-                    Icon(
-                      Icons.check,
-                    )
-                  ],
+    return Container(
+      color: Palette.background100,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 90,
+                width: 90,
+                decoration: const BoxDecoration(
+                  color: Colors.orange,
+                  shape: BoxShape.circle,
                 ),
-                Row(
-                  children: [
-                    Text(
-                      '구독자 n명 · 게시글 n개',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Palette.text300,
+              ),
+              const SizedBox(width: 15),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        '인포팀',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                  ],
+                      SizedBox(width: 5),
+                      Icon(
+                        Icons.check,
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '구독자 n명 · 게시글 n개',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Palette.text300,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+          const SizedBox(height: 15),
+          Container(
+            height: 60,
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: const Color(0xFFF7F7F7),
+            ),
+            child: const Column(
+              children: [
+                Text(
+                  '지속 가능한 개발 문화를 통해 지스트 학부생의 삶의 질을 높이는 팀, 인포팀입니다.',
+                  style: TextStyle(
+                    color: Palette.text300,
+                    fontSize: 14,
+                  ),
                 ),
               ],
-            )
-          ],
-        ),
-        const SizedBox(height: 15),
-        Container(
-          height: 60,
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: const Color(0xFFF7F7F7),
+            ),
           ),
-          child: const Column(
+          const SizedBox(height: 15),
+          Row(
             children: [
-              Text(
-                '지속 가능한 개발 문화를 통해 지스트 학부생의 삶의 질을 높이는 팀, 인포팀입니다.',
-                style: TextStyle(
-                  color: Palette.text300,
-                  fontSize: 14,
+              Expanded(
+                child: ZiggleButton2(
+                  onPressed: () {},
+                  cta: true,
+                  child: Text(t.group.detail.subscribe),
                 ),
               ),
             ],
           ),
-        ),
-        const SizedBox(height: 15),
-        Row(
-          children: [
-            Expanded(
-              child: ZiggleButton2(
-                onPressed: () {},
-                cta: true,
-                child: const Text('구독'),
-              ),
-            ),
-          ],
-        ),
-      ],
+        ],
+      ),
     );
   }
 
