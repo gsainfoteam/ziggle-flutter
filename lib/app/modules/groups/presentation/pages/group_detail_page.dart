26c7486d585881bfce8add069209a97c58be5dfd
dawnfire05 <dawnfire05@naver.com>
2024-08-31 19:48:31 +0900

    fixed: scroll action now flawlessly done

--- lib/app/modules/groups/presentation/pages/group_detail_page.dart
+++ lib/app/modules/groups/presentation/pages/group_detail_page.dart
@@ -8,113 +8,21 @@ class GroupDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Palette.background100,
+        leadingWidth: Theme.of(context).appBarTheme.toolbarHeight,
+        title: Text(t.group.title),
+      ),
       body: DefaultTabController(
         length: 3,
-        child: NestedScrollView(
-          headerSliverBuilder: (context, innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-                leadingWidth: Theme.of(context).appBarTheme.toolbarHeight,
-                title: Text(t.group.title),
-                pinned: true,
-              ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: CustomScrollView(
+            slivers: [
               SliverPersistentHeader(
-                pinned: true,
-                delegate: GroupInfoHeaderDelegate(
-                  column: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Container(
-                            height: 90,
-                            width: 90,
-                            decoration: const BoxDecoration(
-                              color: Colors.orange,
-                              shape: BoxShape.circle,
-                            ),
-                          ),
-                          const SizedBox(width: 15),
-                          const Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                                  Text(
-                                    '인포팀',
-                                    style: TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  SizedBox(width: 5),
-                                  Icon(
-                                    Icons.check,
-                                  )
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Text(
-                                    '구독자 n명 · 게시글 n개',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Palette.text300,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                      const SizedBox(height: 15),
-                      Container(
-                        height: 60,
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: const Color(0xFFF7F7F7),
-                        ),
-                        child: const Column(
-                          children: [
-                            Text(
-                              '지속 가능한 개발 문화를 통해 지스트 학부생의 삶의 질을 높이는 팀, 인포팀입니다.',
-                              style: TextStyle(
-                                color: Palette.text300,
-                                fontSize: 14,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 15),
-                      Container(
-                        height: 51,
-                        decoration: BoxDecoration(
-                          color: Palette.primary100,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            GestureDetector(
-                              onTap: () {},
-                              child: const Text(
-                                '구독',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  color: Palette.white,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                pinned: false,
+                floating: true,
+                delegate: GroupInfoHeaderDelegate(),
               ),
               SliverPersistentHeader(
                 pinned: true,
@@ -143,25 +51,52 @@ class GroupDetailPage extends StatelessWidget {
                     ],
                   ),
                 ),
-              )
-            ];
-          },
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              children: [
-                const SizedBox(height: 20),
-                Expanded(
-                  child: TabBarView(
-                    children: [
-                      Container(child: const Text('소개 내용입니다.소개 내용입니다.')),
-                      Container(child: const Text('공지 내용입니다.')),
-                      Container(child: const Text('멤버 내용입니다.')),
-                    ],
-                  ),
+              ),
+              SliverFillRemaining(
+                child: TabBarView(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                        ],
+                      ),
+                    ),
+                    const Placeholder(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                          Text('소개 내용입니다.소개 내용입니다.'),
+                        ],
+                      ),
+                    ),
+                    Container(child: const Text('멤버 내용입니다.')),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
@@ -170,12 +105,8 @@ class GroupDetailPage extends StatelessWidget {
 }
 
 class GroupInfoHeaderDelegate extends SliverPersistentHeaderDelegate {
-  final Column column;
-
-  GroupInfoHeaderDelegate({required this.column});
-
   @override
-  double get minExtent => 0;
+  double get minExtent => 250;
 
   @override
   double get maxExtent => 250;
@@ -183,9 +114,97 @@ class GroupInfoHeaderDelegate extends SliverPersistentHeaderDelegate {
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: column,
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              height: 90,
+              width: 90,
+              decoration: const BoxDecoration(
+                color: Colors.orange,
+                shape: BoxShape.circle,
+              ),
+            ),
+            const SizedBox(width: 15),
+            const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      '인포팀',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                    Icon(
+                      Icons.check,
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '구독자 n명 · 게시글 n개',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Palette.text300,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+        const SizedBox(height: 15),
+        Container(
+          height: 60,
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: const Color(0xFFF7F7F7),
+          ),
+          child: const Column(
+            children: [
+              Text(
+                '지속 가능한 개발 문화를 통해 지스트 학부생의 삶의 질을 높이는 팀, 인포팀입니다.',
+                style: TextStyle(
+                  color: Palette.text300,
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 15),
+        Container(
+          height: 51,
+          decoration: BoxDecoration(
+            color: Palette.primary100,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {},
+                child: const Text(
+                  '구독',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Palette.white,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 
@@ -209,14 +228,11 @@ class MyTabBarDelegate extends SliverPersistentHeaderDelegate {
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Container(
-        decoration: const BoxDecoration(
-          color: Colors.white,
-        ),
-        child: tabBar,
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
       ),
+      child: tabBar,
     );
   }
 
