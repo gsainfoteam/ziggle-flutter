6773c72591557746089b8543ce1e13e444239556
2paperstar <lee@paperst.ar>
2024-09-06 08:21:32 +0200

    feat: click select menu item action

--- lib/app/modules/common/presentation/widgets/ziggle_select.dart
+++ lib/app/modules/common/presentation/widgets/ziggle_select.dart
@@ -2,22 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:ziggle/app/values/palette.dart';
 import 'package:ziggle/gen/assets.gen.dart';
 
-class ZiggleSelect extends StatefulWidget {
+class ZiggleSelectEntry<T> {
+  final T value;
+  final String label;
+
+  ZiggleSelectEntry({required this.value, required this.label});
+}
+
+class ZiggleSelect<T> extends StatefulWidget {
   const ZiggleSelect({
     super.key,
-    required this.small,
+    this.small = false,
+    required this.hintText,
+    required this.entries,
+    this.onChanged,
   });
 
   final bool small;
+  final String hintText;
+  final List<ZiggleSelectEntry<T>> entries;
+  final void Function(T?)? onChanged;
 
   @override
   State<ZiggleSelect> createState() => _ZiggleSelectState();
 }
 
-class _ZiggleSelectState extends State<ZiggleSelect> {
+class _ZiggleSelectState<T> extends State<ZiggleSelect<T>> {
   final _overlayController = OverlayPortalController();
   final _link = LayerLink();
   double? _buttonWidth;
+  ZiggleSelectEntry<T>? _value;
 
   @override
   Widget build(BuildContext context) {
@@ -48,29 +62,56 @@ class _ZiggleSelectState extends State<ZiggleSelect> {
         borderRadius: BorderRadius.all(Radius.circular(10)),
         color: Palette.white,
       ),
-      child: SizedBox(
-        height: 200,
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxHeight: 200),
         child: Scrollbar(
           thumbVisibility: true,
           child: ListView.builder(
+            shrinkWrap: true,
             padding: EdgeInsets.zero,
-            // shrinkWrap: true,
-            itemBuilder: (context, index) => GestureDetector(
-              onTap: _overlayController.hide,
-              behavior: HitTestBehavior.translucent,
-              child: const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12.5),
-                child: Text(
-                  'asdf',
-                  style: TextStyle(
-                    color: Palette.grayText,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
+            itemCount: widget.entries.length + 1,
+            itemBuilder: (context, index) {
+              final item =
+                  index == 0 ? null : widget.entries.elementAt(index - 1);
+              return GestureDetector(
+                onTap: () {
+                  widget.onChanged?.call(item?.value);
+                  setState(() => _value = item);
+                  _overlayController.hide();
+                },
+                behavior: HitTestBehavior.translucent,
+                child: Padding(
+                  padding: widget.small
+                      ? const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 5) +
+                          const EdgeInsets.only(left: 2)
+                      : const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 12.5) +
+                          const EdgeInsets.only(left: 6),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        item?.label ?? widget.hintText,
+                        style: TextStyle(
+                          color: item == null
+                              ? Palette.grayText
+                              : item == _value
+                                  ? Palette.primary
+                                  : Palette.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      if (item != null && item == _value)
+                        widget.small
+                            ? Assets.icons.check.svg(width: 20, height: 20)
+                            : Assets.icons.check.svg(width: 24, height: 24),
+                    ],
                   ),
                 ),
-              ),
-            ),
-            itemCount: 30,
+              );
+            },
           ),
         ),
       ),
@@ -84,7 +125,11 @@ class _ZiggleSelectState extends State<ZiggleSelect> {
         _overlayController.show();
       },
       child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        padding: widget.small
+            ? const EdgeInsets.symmetric(vertical: 5, horizontal: 10) +
+                const EdgeInsets.only(left: 2)
+            : const EdgeInsets.symmetric(vertical: 12, horizontal: 10) +
+                const EdgeInsets.only(left: 6),
         decoration: const BoxDecoration(
           color: Palette.grayLight,
           borderRadius: BorderRadius.all(Radius.circular(10)),
@@ -92,15 +137,18 @@ class _ZiggleSelectState extends State<ZiggleSelect> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Text(
-              'asdf',
+            Text(
+              _value?.label ?? widget.hintText,
               style: TextStyle(
-                color: Palette.grayText,
+                color: _value == null ? Palette.grayText : Palette.primary,
                 fontSize: 16,
                 fontWeight: FontWeight.w500,
               ),
             ),
-            Assets.icons.chevronDown.svg(width: 24, height: 24),
+            if (widget.small)
+              Assets.icons.chevronDown.svg(width: 20, height: 20)
+            else
+              Assets.icons.chevronDown.svg(width: 24, height: 24),
           ],
         ),
       ),
--- lib/app/values/palette.dart
+++ lib/app/values/palette.dart
@@ -8,7 +8,7 @@ abstract class Palette {
   static const grayLight = Color(0xFFF8F8F8);
   static const gray = Color(0xFFB3B3B3);
   static const grayBorder = Color(0xFFD6D6D6);
-  static const black = Color(0xFF000000);
+  static const black = Color(0xFF252525);
 
   /// for dropdown default color
   static const grayText = Color(0xFF6E6E73);
