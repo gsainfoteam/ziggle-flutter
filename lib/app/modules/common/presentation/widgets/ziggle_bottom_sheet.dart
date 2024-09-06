9637bef2fbff9e0a4c56227e21d3d523863d4e9e
2paperstar <lee@paperst.ar>
2024-09-06 09:17:38 +0200

    feat: add ZiggleBottomSheet

---
+++ lib/app/modules/common/presentation/widgets/ziggle_bottom_sheet.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:ziggle/app/values/palette.dart';
+import 'package:ziggle/gen/assets.gen.dart';
+
+class ZiggleBottomSheet extends StatelessWidget {
+  const ZiggleBottomSheet({
+    super.key,
+    required this.title,
+    required this.child,
+  });
+
+  final String title;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5) +
+            const EdgeInsets.only(bottom: 5),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: 68,
+              height: 5,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Color(0xFFE3E3E3),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      color: Color(0xFF6E6E73),
+                      fontSize: 16,
+                      height: 1.5,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => Navigator.pop(context),
+                    child: Assets.icons.close.svg(width: 14, height: 14),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 15),
+            Flexible(child: child),
+          ],
+        ),
+      ),
+    );
+  }
+
+  static show({
+    required BuildContext context,
+    required String title,
+    required WidgetBuilder builder,
+    bool isDismissible = true,
+    bool enableDrag = true,
+  }) =>
+      showModalBottomSheet(
+        backgroundColor: Palette.white,
+        context: context,
+        isDismissible: isDismissible,
+        isScrollControlled: true,
+        enableDrag: enableDrag,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(14)),
+        ),
+        builder: (context) => ZiggleBottomSheet(
+          title: title,
+          child: builder(context),
+        ),
+      );
+}
