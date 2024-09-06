5e276bb607f701f5ce38b3bd2aa855c95cb00709
Boseong <lee@paperst.ar>
2024-09-06 14:27:28 +0900

    feat: add ziggle button (#283)
    * feat: add ziggle button
    
    * chore(ci/cd): run test for development-v4 branch

---
+++ lib/app/values/palette.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+abstract class Palette {
+  Palette._();
+
+  static const primary = Color(0xFFFF4500);
+  static const white = Color(0xFFFFFFFF);
+  static const grayLight = Color(0xFFF8F8F8);
+  static const gray = Color(0xFFB3B3B3);
+  static const grayBorder = Color(0xD6D6D6FF);
+  static const black = Color(0xFF000000);
+}
