69ae7e654751659c49304acacc0f359093c77421
2paperstar <lee@paperst.ar>
2024-09-07 21:26:26 +0200

    feat: initial app setting

---
+++ lib/app/router/routes.dart
@@ -0,0 +1,9 @@
+import 'package:go_router/go_router.dart';
+
+abstract class AppRoutes {
+  AppRoutes._();
+
+  static final config = GoRouter(
+    routes: [],
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_input.dart';
+import 'package:ziggle/app/app.dart';
+import 'package:ziggle/gen/strings.g.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: ZiggleInput(),
-        ),
-      ),
-    );
-  }
+  runApp(TranslationProvider(child: const App()));
 }
