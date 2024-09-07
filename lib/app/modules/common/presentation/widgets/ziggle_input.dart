7b03800325133f467dbaccf05fb279522b1b4ce1
dawnfire05 <dawnfire05@naver.com>
2024-09-07 21:53:27 +0900

    fix: resolved suggestions

--- lib/app/modules/common/presentation/widgets/ziggle_input.dart
+++ lib/app/modules/common/presentation/widgets/ziggle_input.dart
@@ -4,12 +4,12 @@ import 'package:ziggle/app/values/palette.dart';
 class ZiggleInput extends StatefulWidget {
   const ZiggleInput({
     super.key,
-    required this.c,
+    required this.controller,
     this.disabled = false,
     required this.hintText,
   });
 
-  final TextEditingController c;
+  final TextEditingController controller;
   final bool disabled;
   final String hintText;
 
@@ -18,11 +18,11 @@ class ZiggleInput extends StatefulWidget {
 }
 
 class _ZiggleInputState extends State<ZiggleInput> {
-  OutlineInputBorder _buildInputBorder() {
+  OutlineInputBorder _buildInputBorder(Color color) {
     return OutlineInputBorder(
       borderSide: BorderSide(
         width: 1.5,
-        color: widget.disabled ? Palette.grayBorder : Palette.primary,
+        color: color,
       ),
       borderRadius: BorderRadius.circular(10),
     );
@@ -33,16 +33,16 @@ class _ZiggleInputState extends State<ZiggleInput> {
     return SizedBox(
       height: 48,
       child: TextFormField(
-        controller: widget.c,
-        style: const TextStyle(fontWeight: FontWeight.bold),
+        controller: widget.controller,
         readOnly: widget.disabled ? true : false,
         decoration: InputDecoration(
           contentPadding:
               const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
           hintText: widget.hintText,
-          hintStyle: const TextStyle(color: Palette.lightGray),
-          enabledBorder: _buildInputBorder(),
-          focusedBorder: _buildInputBorder(),
+          hintStyle: const TextStyle(color: Palette.gray),
+          enabledBorder: _buildInputBorder(Palette.gray),
+          focusedBorder: _buildInputBorder(
+              widget.disabled ? Palette.gray : Palette.primary),
         ),
       ),
     );
