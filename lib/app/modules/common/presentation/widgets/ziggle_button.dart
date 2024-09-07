7e60f88fc5ae2e2dd4875e87e07df6140be27cad
2paperstar <lee@paperst.ar>
2024-09-07 09:23:13 +0200

    refactor: separate ZigglePressable widget

--- lib/app/modules/common/presentation/widgets/ziggle_button.dart
+++ lib/app/modules/common/presentation/widgets/ziggle_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_pressable.dart';
 import 'package:ziggle/app/values/palette.dart';
 
 enum ZiggleButtonType {
@@ -11,7 +12,7 @@ enum ZiggleButtonType {
   const ZiggleButtonType(this.padding);
 }
 
-class ZiggleButton extends StatefulWidget {
+class ZiggleButton extends StatelessWidget {
   const ZiggleButton({
     super.key,
     required this.child,
@@ -31,36 +32,27 @@ class ZiggleButton extends StatefulWidget {
   final bool emphasize;
   final ZiggleButtonType type;
 
-  @override
-  State<ZiggleButton> createState() => _ZiggleButtonState();
-}
-
-class _ZiggleButtonState extends State<ZiggleButton> {
-  bool _pressed = false;
-  bool _active = false;
-  bool get pressed => _pressed || _active;
-
   Color get _effectiveColor {
-    if (widget.type == ZiggleButtonType.text) return Palette.primary;
-    if (widget.disabled) return Palette.gray;
-    if (widget.outlined) return Palette.primary;
-    if (widget.emphasize) return Palette.white;
+    if (type == ZiggleButtonType.text) return Palette.primary;
+    if (disabled) return Palette.gray;
+    if (outlined) return Palette.primary;
+    if (emphasize) return Palette.white;
     return Palette.black;
   }
 
   Color? get _effectiveBackgroundColor {
-    if (widget.type == ZiggleButtonType.text) return null;
-    if (widget.disabled) return Palette.grayLight;
-    if (widget.outlined) return null;
-    if (widget.emphasize) return Palette.primary;
+    if (type == ZiggleButtonType.text) return null;
+    if (disabled) return Palette.grayLight;
+    if (outlined) return null;
+    if (emphasize) return Palette.primary;
     return Palette.grayLight;
   }
 
   Color? get _effectiveBorderColor {
-    if (widget.type == ZiggleButtonType.text) return null;
-    if (widget.disabled) return null;
-    if (widget.outlined) return Palette.primary;
-    if (widget.emphasize) return null;
+    if (type == ZiggleButtonType.text) return null;
+    if (disabled) return null;
+    if (outlined) return Palette.primary;
+    if (emphasize) return null;
     return Palette.grayBorder;
   }
 
@@ -71,52 +63,34 @@ class _ZiggleButtonState extends State<ZiggleButton> {
         fontSize: 18,
         fontWeight: FontWeight.bold,
       ).copyWith(color: _effectiveColor),
-      child: widget.child,
+      child: child,
     );
 
-    return GestureDetector(
-      onTapDown: (_) {
-        if (widget.disabled) return;
-        setState(() {
-          _pressed = true;
-          _active = true;
-        });
-        Future.delayed(const Duration(milliseconds: 100), () {
-          if (!mounted) return;
-          setState(() => _active = false);
-        });
-      },
-      onTapUp: (_) => setState(() => _pressed = false),
-      onTapCancel: () => setState(() => _pressed = false),
-      onTap: widget.loading ? null : widget.onPressed,
-      child: AnimatedScale(
+    return ZigglePressable(
+      onPressed: disabled || loading ? null : onPressed,
+      decoration: BoxDecoration(
+        color: _effectiveBackgroundColor,
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        border: Border.all(
+          color: _effectiveBorderColor ?? Colors.transparent,
+          style: _effectiveBorderColor == null
+              ? BorderStyle.none
+              : BorderStyle.solid,
+        ),
+      ),
+      child: AnimatedContainer(
         duration: const Duration(milliseconds: 100),
-        scale: widget.onPressed != null && pressed ? 0.95 : 1,
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 100),
-          padding: widget.type.padding,
-          width: widget.type == ZiggleButtonType.cta ? double.infinity : null,
-          decoration: BoxDecoration(
-            color: _effectiveBackgroundColor
-                ?.withOpacity(widget.onPressed != null && pressed ? 0.8 : 1),
-            borderRadius: const BorderRadius.all(Radius.circular(10)),
-            border: Border.all(
-              color: _effectiveBorderColor ?? Colors.transparent,
-              style: _effectiveBorderColor == null
-                  ? BorderStyle.none
-                  : BorderStyle.solid,
+        padding: type.padding,
+        width: type == ZiggleButtonType.cta ? double.infinity : null,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Opacity(opacity: loading ? 0 : 1, child: inner),
+            Opacity(
+              opacity: loading ? 1 : 0,
+              child: const CircularProgressIndicator(),
             ),
-          ),
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Opacity(opacity: widget.loading ? 0 : 1, child: inner),
-              Opacity(
-                opacity: widget.loading ? 1 : 0,
-                child: const CircularProgressIndicator(),
-              ),
-            ],
-          ),
+          ],
         ),
       ),
     );
