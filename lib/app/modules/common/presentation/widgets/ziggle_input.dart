35759b3675b6b0c8aec964a921c7b0cb599189ac
dawnfire05 <dawnfire05@naver.com>
2024-09-07 18:26:07 +0900

    test: branch test

---
+++ lib/app/modules/common/presentation/widgets/ziggle_input.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ZiggleInput extends StatelessWidget {
+  const ZiggleInput({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_input.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: ZiggleInput(),
         ),
       ),
     );
