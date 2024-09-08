e49886d12148ee8b4542e6a6eed5bf88b081b585
dawnfire05 <dawnfire05@naver.com>
2024-09-08 20:32:39 +0900

    refactor: turn it into a stateless widget

--- lib/app/modules/common/presentation/widgets/ziggle_input.dart
+++ lib/app/modules/common/presentation/widgets/ziggle_input.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ziggle/app/values/palette.dart';
 
-class ZiggleInput extends StatefulWidget {
+class ZiggleInput extends StatelessWidget {
   const ZiggleInput({
     super.key,
     required this.controller,
@@ -13,11 +13,6 @@ class ZiggleInput extends StatefulWidget {
   final bool disabled;
   final String hintText;
 
-  @override
-  State<ZiggleInput> createState() => _ZiggleInputState();
-}
-
-class _ZiggleInputState extends State<ZiggleInput> {
   OutlineInputBorder _buildInputBorder(Color color) {
     return OutlineInputBorder(
       borderSide: BorderSide(
@@ -33,16 +28,16 @@ class _ZiggleInputState extends State<ZiggleInput> {
     return SizedBox(
       height: 48,
       child: TextFormField(
-        controller: widget.controller,
-        readOnly: widget.disabled ? true : false,
+        controller: controller,
+        readOnly: disabled ? true : false,
         decoration: InputDecoration(
           contentPadding:
               const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-          hintText: widget.hintText,
+          hintText: hintText,
           hintStyle: const TextStyle(color: Palette.gray),
           enabledBorder: _buildInputBorder(Palette.gray),
-          focusedBorder: _buildInputBorder(
-              widget.disabled ? Palette.gray : Palette.primary),
+          focusedBorder:
+              _buildInputBorder(disabled ? Palette.gray : Palette.primary),
         ),
       ),
     );
