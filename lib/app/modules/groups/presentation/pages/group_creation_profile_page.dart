eb48f4d9d20dcbc9f987838671b2eb441f9be87e
2paperstar <lee@paperst.ar>
2024-09-09 08:42:52 +0200

    feat: select profile image

--- lib/app/modules/groups/presentation/pages/group_creation_profile_page.dart
+++ lib/app/modules/groups/presentation/pages/group_creation_profile_page.dart
@@ -1,22 +1,49 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_button.dart';
 import 'package:ziggle/app/router/routes.dart';
 import 'package:ziggle/gen/assets.gen.dart';
 import 'package:ziggle/gen/strings.g.dart';
 
-class GroupCreationProfilePage extends StatelessWidget {
+class GroupCreationProfilePage extends StatefulWidget {
   const GroupCreationProfilePage({super.key});
+
+  @override
+  State<GroupCreationProfilePage> createState() =>
+      _GroupCreationProfilePageState();
+}
+
+class _GroupCreationProfilePageState extends State<GroupCreationProfilePage> {
+  File? _image;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         const SizedBox(height: 40),
-        Assets.images.groupDefaultProfile.image(width: 300),
+        if (_image != null)
+          SizedBox(
+            width: 300,
+            height: 300,
+            child: Image.file(
+              _image!,
+              fit: BoxFit.cover,
+            ),
+          )
+        else
+          Assets.images.groupDefaultProfile.image(width: 300),
         const SizedBox(height: 27),
         IntrinsicWidth(
           child: ZiggleButton(
             emphasize: false,
+            onPressed: () async {
+              final image =
+                  await ImagePicker().pickImage(source: ImageSource.gallery);
+              if (image == null) return;
+              setState(() => _image = File(image.path));
+            },
             child: Text(t.group.creation.setProfileImage),
           ),
         ),
