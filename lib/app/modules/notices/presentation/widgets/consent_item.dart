f853aecea881b6e5cd1daf97afbf838bd22b3dc6
dawnfire05 <dawnfire05@naver.com>
2024-09-10 11:34:17 +0900

    feat: implement checkbox and color change

--- lib/app/modules/notices/presentation/widgets/consent_item.dart
+++ lib/app/modules/notices/presentation/widgets/consent_item.dart
@@ -1,46 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_checkbox.dart';
 import 'package:ziggle/app/values/palette.dart';
 
-class ConsentItem extends StatelessWidget {
+class ConsentItem extends StatefulWidget {
   const ConsentItem({
     super.key,
     required this.title,
     required this.description,
+    this.isChecked = false,
   });
 
   final String title;
   final String description;
+  final bool isChecked;
+
+  @override
+  State<ConsentItem> createState() => _ConsentItemState();
+}
+
+class _ConsentItemState extends State<ConsentItem> {
+  late bool isChecked;
+
+  @override
+  void initState() {
+    isChecked = widget.isChecked;
+    super.initState();
+  }
+
+  _toggleCheck(bool newValue) {
+    setState(() {
+      isChecked = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: ShapeDecoration(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))),
+        color: isChecked ? Palette.primaryLight : Palette.grayLight,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
       padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 14),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Container(
-            color: Palette.primary,
-            height: 24,
-            width: 24,
+          ZiggleCheckbox(
+            onToggle: (newValue) => _toggleCheck(newValue),
           ),
-          Column(
-            children: [
-              Text(
-                title,
-                style: const TextStyle(
-                  color: Palette.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w700,
-                  height: 0.08,
-                  letterSpacing: -0.45,
+          const SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  widget.title,
+                  style: const TextStyle(
+                    color: Palette.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: -0.45,
+                  ),
                 ),
-              ),
-              Text(description)
-            ],
+                Text(
+                  widget.description,
+                )
+              ],
+            ),
           )
         ],
       ),
