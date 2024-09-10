fb21aff6871486fad44e0584f817707a99f7be51
이신혁 <69188972+dawnfire05@users.noreply.github.com>
2024-09-10 23:01:14 +0900

    feat: add ZiggleInput (#317)
    * feat: implemented ziggle input button
    
    * fix: removed button
    
    * fix: main file redo
    
    * revert: main.dart
    
    * fix: resolved suggestions
    
    * refactor: turn it into a stateless widget

--- lib/app/modules/common/presentation/widgets/ziggle_input.dart
+++ lib/app/modules/common/presentation/widgets/ziggle_input.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:ziggle/app/values/palette.dart';
 
 class ZiggleInput extends StatelessWidget {
-  const ZiggleInput({super.key});
+  const ZiggleInput({
+    super.key,
+    required this.controller,
+    this.disabled = false,
+    required this.hintText,
+  });
+
+  final TextEditingController controller;
+  final bool disabled;
+  final String hintText;
+
+  OutlineInputBorder _buildInputBorder(Color color) {
+    return OutlineInputBorder(
+      borderSide: BorderSide(
+        width: 1.5,
+        color: color,
+      ),
+      borderRadius: BorderRadius.circular(10),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      height: 48,
+      child: TextFormField(
+        controller: controller,
+        readOnly: disabled ? true : false,
+        decoration: InputDecoration(
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+          hintText: hintText,
+          hintStyle: const TextStyle(color: Palette.gray),
+          enabledBorder: _buildInputBorder(Palette.gray),
+          focusedBorder:
+              _buildInputBorder(disabled ? Palette.gray : Palette.primary),
+        ),
+      ),
+    );
   }
 }
