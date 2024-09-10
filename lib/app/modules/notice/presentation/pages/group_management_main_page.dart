102124008c7d76fc1666487a4fb97c712f9fd40b
dawnfire05 <dawnfire05@naver.com>
2024-09-10 17:38:53 +0900

    feat: init

---
+++ lib/app/modules/notice/presentation/pages/group_management_main_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GroupManagementMainPage extends StatelessWidget {
+  const GroupManagementMainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center());
+  }
+}
