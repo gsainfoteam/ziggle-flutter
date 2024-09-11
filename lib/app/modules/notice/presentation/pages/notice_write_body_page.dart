e0fff0b33348de131e1e87e7f27c2f08a6a420dc
2paperstar <lee@paperst.ar>
2024-09-11 18:45:42 +0900

    feat: add appbar in notice write body page

--- lib/app/modules/notice/presentation/pages/notice_write_body_page.dart
+++ lib/app/modules/notice/presentation/pages/notice_write_body_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_app_bar.dart';
+import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_back_button.dart';
+import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_button.dart';
+import 'package:ziggle/gen/strings.g.dart';
 
 class NoticeWriteBodyPage extends StatelessWidget {
   const NoticeWriteBodyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: ZiggleAppBar(
+        leading: ZiggleBackButton(label: t.common.cancel),
+        title: Text(t.notice.write.title),
+        actions: [
+          ZiggleButton(
+            type: ZiggleButtonType.text,
+            child: Text(
+              t.common.done,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
