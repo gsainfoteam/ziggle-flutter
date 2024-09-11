981ba870d9caa7df5c3f0f93692615c3edd1c217
Boseong <paperstar@gm.gist.ac.kr>
2024-09-11 18:33:05 +0900

    feat: group creation sequence UI (#326)
    * feat: add group creation layout
    
    * fix: compact ZiggleAppBar align
    
    * fix: change back button behavior (maybePop -> pop)
    
    * fix: rename ZiggleProgressBar widget
    
    * feat: GroupCreationLayout UI
    
    * feat: group profile UI
    
    * fix: AppBar height
    
    * feat: add GroupCreationIntroducePage
    
    * fix: change layout padding
    
    * feat: GroupCreationIntroducePage UI
    
    * feat: add done, notion page
    
    * chore(deps): downgrade go_router 12.1.3
    
    * feat: navigate from notion page
    
    * chore: remove unused commented code
    
    * feat: done page UI
    
    * feat: button style
    
    * feat: add loading notion box
    
    * chore(deps): add image_picker
    
    * chore: update Podfile.lock
    
    * feat: select profile image
    
    * feat: fill group name
    
    * feat: fill introduce content
    
    * feat: add text field in notion page
    
    * fix: ZiggleSelect type generic
    
    * feat: group creation done page generation state
    
    * feat: add label widget in ZiggleInput
    
    * feat: group name with ZiggleInput
    
    * feat: notion link with ZiggleInput
    
    * chore: revert splash page

--- lib/app/modules/common/presentation/widgets/ziggle_input.dart
+++ lib/app/modules/common/presentation/widgets/ziggle_input.dart
@@ -4,14 +4,18 @@ import 'package:ziggle/app/values/palette.dart';
 class ZiggleInput extends StatelessWidget {
   const ZiggleInput({
     super.key,
-    required this.controller,
+    this.controller,
     this.disabled = false,
     required this.hintText,
+    this.onChanged,
+    this.label,
   });
 
-  final TextEditingController controller;
+  final TextEditingController? controller;
   final bool disabled;
   final String hintText;
+  final void Function(String)? onChanged;
+  final Widget? label;
 
   OutlineInputBorder _buildInputBorder(Color color) {
     return OutlineInputBorder(
@@ -25,21 +29,38 @@ class ZiggleInput extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 48,
-      child: TextFormField(
-        controller: controller,
-        readOnly: disabled ? true : false,
-        decoration: InputDecoration(
-          contentPadding:
-              const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-          hintText: hintText,
-          hintStyle: const TextStyle(color: Palette.gray),
-          enabledBorder: _buildInputBorder(Palette.gray),
-          focusedBorder:
-              _buildInputBorder(disabled ? Palette.gray : Palette.primary),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        if (label != null) ...[
+          DefaultTextStyle.merge(
+            style: const TextStyle(
+              color: Palette.black,
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+            child: label!,
+          ),
+          const SizedBox(height: 10),
+        ],
+        SizedBox(
+          height: 48,
+          child: TextFormField(
+            controller: controller,
+            readOnly: disabled ? true : false,
+            onChanged: onChanged,
+            decoration: InputDecoration(
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+              hintText: hintText,
+              hintStyle: const TextStyle(color: Palette.gray),
+              enabledBorder: _buildInputBorder(Palette.gray),
+              focusedBorder:
+                  _buildInputBorder(disabled ? Palette.gray : Palette.primary),
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
