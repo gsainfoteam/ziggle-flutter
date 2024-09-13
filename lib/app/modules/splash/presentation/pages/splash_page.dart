57297f38edff1d7b0e606a6e06c018996057ea3c
Boseong <paperstar@gm.gist.ac.kr>
2024-09-14 00:21:28 +0900

    feat: add bottom navigator (#337)
    * feat: add bottom navigator
    
    * chore: add feed page routing button in splash screen

--- lib/app/modules/splash/presentation/pages/splash_page.dart
+++ lib/app/modules/splash/presentation/pages/splash_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_button.dart';
+import 'package:ziggle/app/router/routes.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox.shrink();
+    return ZiggleButton.cta(
+      onPressed: () => const FeedRoute().go(context),
+      child: const Text('feed page'),
+    );
   }
 }
