575d796369052ac3ad6ec1f4f8a0745a00f65958
2paperstar <lee@paperst.ar>
2024-09-14 03:47:35 +0900

    feat: initialize modules

--- lib/app/di/module.dart
+++ lib/app/di/module.dart
@@ -1,8 +1,31 @@
+import 'package:cookie_jar/cookie_jar.dart';
+import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
+import 'package:path_provider/path_provider.dart';
 
 @module
 abstract class AppModule {
+  @preResolve
+  @singleton
+  Future<CookieJar> makeCookieJar() async {
+    final appDocDir = await getApplicationDocumentsDirectory();
+    final appDocPath = appDocDir.path;
+    return PersistCookieJar(
+      ignoreExpires: true,
+      storage: FileStorage("$appDocPath/.cookies/"),
+    );
+  }
+
+  @singleton
+  CookieManager getCookieManager(CookieJar cookieJar) =>
+      CookieManager(cookieJar);
+
+  @singleton
+  Dio getDio(CookieManager cookieManager) =>
+      Dio()..interceptors.addAll([cookieManager]);
+
   FlutterSecureStorage get flutterSecureStorage => const FlutterSecureStorage(
         aOptions: AndroidOptions(encryptedSharedPreferences: true),
       );
