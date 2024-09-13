ba740c4ddc489c1a5e39df01154ce5322ce63695
2paperstar <lee@paperst.ar>
2024-09-13 23:02:00 +0900

    chore: revert splash page change

--- lib/app/modules/splash/presentation/pages/splash_page.dart
+++ lib/app/modules/splash/presentation/pages/splash_page.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_button.dart';
-import 'package:ziggle/app/router/routes.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ZiggleButton.cta(
-      onPressed: () => const NoticeWriteRoute().push(context),
-      child: const Text('asdf'),
-    );
+    return const SizedBox.shrink();
   }
 }
