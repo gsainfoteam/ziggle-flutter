87fde231ad0efd699a94289a9008a63af64bae90
2paperstar <lee@paperst.ar>
2024-09-13 19:20:29 +0900

    feat: initialize locale

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:ziggle/app/app.dart';
 import 'package:ziggle/gen/strings.g.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await _initLocale();
   runApp(TranslationProvider(child: const App()));
+}
+
+Future<void> _initLocale() async {
+  final locale = LocaleSettings.useDeviceLocale();
+  LocaleSettings.setLocale(locale);
+  await initializeDateFormatting();
+  Intl.defaultLocale = locale.languageCode;
+  LocaleSettings.setPluralResolver(
+    locale: AppLocale.ko,
+    cardinalResolver: (n, {few, many, one, other, two, zero}) =>
+        other ?? n.toString(),
+    ordinalResolver: (n, {few, many, one, other, two, zero}) =>
+        other ?? n.toString(),
+  );
 }
