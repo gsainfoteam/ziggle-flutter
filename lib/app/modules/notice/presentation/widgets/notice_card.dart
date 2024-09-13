e21dafebca67ad4963a95dddb7cd9f1dbba7a199
Boseong <paperstar@gm.gist.ac.kr>
2024-09-14 00:49:43 +0900

    feat: add NoticeCard (#338)
    * feat: notice card UI
    
    * feat: show notice content with NoticeSummary entity
    
    * feat: add DDay widget
    
    * feat: add behavior property in ZigglePressable
    
    * feat: add press events in NoticeCard

---
+++ lib/app/modules/notice/presentation/widgets/notice_card.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_pressable.dart';
+import 'package:ziggle/app/modules/notice/domain/entities/notice_summary.dart';
+import 'package:ziggle/app/modules/notice/presentation/widgets/d_day.dart';
+import 'package:ziggle/app/values/palette.dart';
+import 'package:ziggle/gen/assets.gen.dart';
+
+class NoticeCard extends StatelessWidget {
+  const NoticeCard({
+    super.key,
+    required this.notice,
+    required this.onPressed,
+    required this.onLike,
+    required this.onShare,
+  });
+
+  final NoticeSummary notice;
+  final VoidCallback onPressed;
+  final VoidCallback onLike;
+  final VoidCallback onShare;
+
+  @override
+  Widget build(BuildContext context) {
+    return ZigglePressable(
+      onPressed: onPressed,
+      decoration: const BoxDecoration(
+        color: Palette.white,
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(14),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              children: [
+                Assets.images.defaultProfile.image(width: 40),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            notice.authorName,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: Palette.black,
+                              height: 1,
+                              leadingDistribution: TextLeadingDistribution.even,
+                            ),
+                          ),
+                          const SizedBox(width: 5),
+                          if (notice.authorIsCertificated)
+                            Assets.icons.certificatedBadge.svg(width: 20),
+                        ],
+                      ),
+                      const SizedBox(height: 2),
+                      const Text(
+                        '10분 전',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Palette.gray,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                if (notice.deadline != null) DDay(deadline: notice.deadline!),
+              ],
+            ),
+            const SizedBox(height: 8),
+            Text(
+              notice.title,
+              style: const TextStyle(
+                color: Palette.black,
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 8),
+            if (notice.images.isNotEmpty)
+              Image.network(
+                notice.images.first,
+                height: 250,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              )
+            else
+              Text(
+                notice.content,
+                style: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: Palette.black,
+                ),
+              ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                ZigglePressable(
+                  behavior: HitTestBehavior.translucent,
+                  onPressed: onLike,
+                  child: Row(
+                    children: [
+                      Assets.icons.fire.svg(width: 30),
+                      const SizedBox(width: 5),
+                      Text(
+                        '${notice.likes}',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Palette.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                ZigglePressable(
+                  behavior: HitTestBehavior.translucent,
+                  onPressed: onShare,
+                  child: Assets.icons.share.svg(width: 30),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
