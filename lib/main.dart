c4c0f14ecee46f1ae8ab5cb1027cc44884671ccc
2paperstar <lee@paperst.ar>
2024-09-14 14:56:47 +0900

    feat: init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,10 +7,12 @@ import 'package:intl/intl.dart';
 import 'package:ziggle/app/app.dart';
 import 'package:ziggle/app/di/locator.dart';
 import 'package:ziggle/app_bloc_observer.dart';
+import 'package:ziggle/firebase_options.dart';
 import 'package:ziggle/gen/strings.g.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await configureDependencies();
   await _initLocale();
   _initBloc();
