61a3318411a0c109fd87ef974012508060f129ec
2paperstar <lee@paperst.ar>
2024-09-15 07:00:01 +0900

    fix: notice category enum rename

--- lib/app/modules/notices/domain/enums/notice_category.dart
+++ lib/app/modules/notices/domain/enums/notice_category.dart
@@ -1,5 +1,7 @@
+import 'package:json_annotation/json_annotation.dart';
 import 'package:ziggle/gen/strings.g.dart';
 
+@JsonEnum(fieldRename: FieldRename.screamingSnake)
 enum NoticeCategory {
   academic,
   recruit,
