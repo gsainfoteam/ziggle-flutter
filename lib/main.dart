14413f7483642d0fb54aab19e9af4c73a86fdbca
2paperstar <lee@paperst.ar>
2024-09-14 14:54:52 +0900

    fix: configure dependency

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:ziggle/app/app.dart';
+import 'package:ziggle/app/di/locator.dart';
 import 'package:ziggle/gen/strings.g.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await configureDependencies();
   await _initLocale();
   runApp(TranslationProvider(child: const App()));
 }
