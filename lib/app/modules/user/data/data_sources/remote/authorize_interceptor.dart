a0eeff151fd78dd40d3127dd650f5d9658df6c3c
Boseong <paperstar@gm.gist.ac.kr>
2024-09-15 13:00:55 +0900

    fix: refresh token issue (#353)

--- lib/app/modules/user/data/data_sources/remote/authorize_interceptor.dart
+++ lib/app/modules/user/data/data_sources/remote/authorize_interceptor.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
+import 'package:mutex/mutex.dart';
 import 'package:ziggle/app/di/locator.dart';
 import 'package:ziggle/app/modules/user/data/data_sources/remote/user_api.dart';
 import 'package:ziggle/app/modules/user/domain/repositories/token_repository.dart';
@@ -8,6 +9,7 @@ import 'package:ziggle/app/modules/user/domain/repositories/token_repository.dar
 class AuthorizeInterceptor extends Interceptor {
   final TokenRepository _repository;
   static const retriedKey = '_retried';
+  final mutex = ReadWriteMutex();
 
   AuthorizeInterceptor(this._repository);
 
@@ -19,22 +21,11 @@ class AuthorizeInterceptor extends Interceptor {
     if (statusCode != 401) return handler.next(err);
     final token = await _repository.token.first;
     if (token == null) return handler.next(err);
-    final retried = err.requestOptions.extra.containsKey(retriedKey);
-    if (retried) return handler.next(err);
-    err.requestOptions.extra[retriedKey] = true;
+    if (err.requestOptions.retried) return handler.next(err);
+    err.requestOptions.retried = true;
+
     try {
-      final userApi = sl<UserApi>();
-      try {
-        userApi.testTokenInfo();
-      } catch (e) {
-        try {
-          final token = await userApi.refresh();
-          await _repository.saveToken(token.accessToken);
-        } catch (e) {
-          await _repository.deleteToken();
-          return handler.next(err);
-        }
-      }
+      if (!(await _refresh())) return handler.next(err);
       final retriedResponse = await dio.fetch(err.requestOptions);
       return handler.resolve(retriedResponse);
     } on DioException {
@@ -47,19 +38,47 @@ class AuthorizeInterceptor extends Interceptor {
     RequestOptions options,
     RequestInterceptorHandler handler,
   ) async {
+    if (options.retried) return handler.next(options);
     if (_repository.tokenExpiration != null) {
       if (DateTime.now().isAfter(_repository.tokenExpiration!)) {
-        try {
-          final userApi = sl<UserApi>();
-          final token = await userApi.refresh();
-          await _repository.saveToken(token.accessToken);
-        } catch (_) {}
+        await _refresh();
       }
     }
-    final token = await _repository.token.first;
-    if (token != null) {
-      options.headers['Authorization'] = 'Bearer $token';
+
+    try {
+      await mutex.acquireRead();
+      final token = await _repository.token.first;
+      if (token != null) {
+        options.headers['Authorization'] = 'Bearer $token';
+      }
+      handler.next(options);
+    } finally {
+      mutex.release();
     }
-    handler.next(options);
   }
+
+  Future<bool> _refresh() async {
+    if (mutex.isWriteLocked) {
+      await mutex.acquireRead();
+      mutex.release();
+      return true;
+    }
+    await mutex.acquireWrite();
+    final userApi = sl<UserApi>();
+    try {
+      final token = await userApi.refresh();
+      await _repository.saveToken(token.accessToken);
+      return true;
+    } catch (e) {
+      await _repository.deleteToken();
+      return false;
+    } finally {
+      mutex.release();
+    }
+  }
+}
+
+extension _RequestOptionsX on RequestOptions {
+  bool get retried => extra.containsKey(AuthorizeInterceptor.retriedKey);
+  set retried(bool value) => extra[AuthorizeInterceptor.retriedKey] = value;
 }
