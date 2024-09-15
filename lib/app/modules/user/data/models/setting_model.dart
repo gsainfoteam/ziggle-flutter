d2b8f082951a458c19e31871f51936621228f2f6
2paperstar <lee@paperst.ar>
2024-09-15 14:37:18 +0900

    feat: add language setting repository

---
+++ lib/app/modules/user/data/models/setting_model.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hive/hive.dart';
+import 'package:ziggle/gen/strings.g.dart';
+
+part 'setting_model.freezed.dart';
+part 'setting_model.g.dart';
+
+@HiveType(typeId: 1)
+@freezed
+class SettingModel with _$SettingModel {
+  const SettingModel._();
+
+  const factory SettingModel({
+    @HiveField(0) required String language,
+  }) = _SettingModel;
+
+  factory SettingModel.init() =>
+      SettingModel(language: AppLocaleUtils.findDeviceLocale().name);
+}
