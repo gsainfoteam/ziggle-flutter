3902c3e422f0522adfd64b012a896a63765c3480
2paperstar <lee@paperst.ar>
2024-09-15 15:17:49 +0900

    fix: ziggle row button expanded style

--- lib/app/modules/common/presentation/widgets/ziggle_row_button.dart
+++ lib/app/modules/common/presentation/widgets/ziggle_row_button.dart
@@ -35,15 +35,20 @@ class ZiggleRowButton extends StatelessWidget {
           children: [
             if (icon != null) icon!,
             const SizedBox(width: 5),
-            DefaultTextStyle.merge(
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-                color: destructive ? Palette.primary : Palette.black,
+            Expanded(
+              child: DefaultTextStyle.merge(
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: disabled
+                      ? Palette.gray
+                      : destructive
+                          ? Palette.primary
+                          : Palette.black,
+                ),
+                child: title,
               ),
-              child: title,
             ),
-            const Spacer(),
             if (showChevron) Assets.icons.chevronRight.svg(),
           ],
         ),
