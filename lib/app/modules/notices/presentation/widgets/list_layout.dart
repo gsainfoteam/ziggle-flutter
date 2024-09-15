6dcb64a1d78b3d15eb45a2dc055bb12836bc2c11
Boseong <paperstar@gm.gist.ac.kr>
2024-09-15 18:21:53 +0900

    feat: add notice list, detail api (#352)
    * feat: add notice model, entity, api
    
    * fix: authorized interceptor infinite loop
    
    * fix: await token info
    
    * fix: test isSignedIn with non-retried key
    
    so that this api request intercepted by authorized interceptor
    
    * feat: add notice list bloc
    
    * refactor: remove wrong files
    
    * fix: notice category enum rename
    
    * feat: implement getNotces
    
    * refactor: show loading indicator when loading
    
    * feat: apply same into list page
    
    * refactor: separate ListLayout
    
    * chore: ignore freezed by linter
    
    * feat: add category query in get notices api
    
    * feat: get notices by type and refresh by recent type
    
    * fix: separate get notice query model, add notice sort
    
    * chore(deps): add extended_text
    
    * feat: restrict content in NoticeCard with ExtendedText
    
    * feat: load more with InfiniteScroll
    
    * fix: when load more, set loading state with previous state
    
    * feat: push to notice page
    
    * feat: add notice bloc
    
    * fix: make grayed out deadline if exceeded in detail page
    
    * feat: show notice reactions count

---
+++ lib/app/modules/notices/presentation/widgets/list_layout.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ziggle/app/modules/notices/presentation/bloc/notice_list_bloc.dart';
+import 'package:ziggle/app/modules/notices/presentation/widgets/infinite_scroll.dart';
+import 'package:ziggle/app/modules/notices/presentation/widgets/notice_card.dart';
+import 'package:ziggle/app/router/routes.dart';
+
+class ListLayout extends StatelessWidget {
+  const ListLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<NoticeListBloc, NoticeListState>(
+      builder: (context, state) {
+        return RefreshIndicator(
+          onRefresh: () => NoticeListBloc.refresh(context),
+          child: state.showLoading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : InfiniteScroll(
+                  onLoadMore: () => NoticeListBloc.loadMore(context),
+                  slivers: [
+                    SliverPadding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 16,
+                      ),
+                      sliver: SliverList.separated(
+                        itemBuilder: (context, index) {
+                          if (index >= state.notices.length) {
+                            return const Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(
+                                child: CircularProgressIndicator(),
+                              ),
+                            );
+                          }
+                          final notice = state.notices[index];
+                          return NoticeCard(
+                            onLike: () {},
+                            onPressed: () =>
+                                NoticeDetailRoute.fromEntity(notice)
+                                    .push(context),
+                            onShare: () {},
+                            notice: notice,
+                          );
+                        },
+                        separatorBuilder: (_, __) => const SizedBox(height: 15),
+                        itemCount:
+                            state.notices.length + (state.isLoading ? 1 : 0),
+                      ),
+                    ),
+                  ],
+                ),
+        );
+      },
+    );
+  }
+}
