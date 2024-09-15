1e1b59227338fe02e273c4d6cd070d0f2de675d1
2paperstar <lee@paperst.ar>
2024-09-15 15:31:04 +0900

    feat: show login button in setting page

--- lib/app/modules/user/presentation/pages/setting_page.dart
+++ lib/app/modules/user/presentation/pages/setting_page.dart
@@ -3,10 +3,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 import 'package:ziggle/app/di/locator.dart';
 import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_app_bar.dart';
+import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_button.dart';
 import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_row_button.dart';
 import 'package:ziggle/app/modules/user/domain/repositories/language_setting_repository.dart';
 import 'package:ziggle/app/modules/user/domain/repositories/notification_setting_repository.dart';
 import 'package:ziggle/app/modules/user/presentation/bloc/auth_bloc.dart';
+import 'package:ziggle/app/modules/user/presentation/bloc/user_bloc.dart';
 import 'package:ziggle/app/router/routes.dart';
 import 'package:ziggle/app/values/palette.dart';
 import 'package:ziggle/app/values/strings.dart';
@@ -29,20 +31,34 @@ class SettingPage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               _Title(title: context.t.user.account.title),
-              ZiggleRowButton(
-                title: Text(context.t.user.account.logout),
-                destructive: true,
-                showChevron: false,
-                onPressed: () =>
-                    context.read<AuthBloc>().add(const AuthEvent.logout()),
-              ),
-              const SizedBox(height: 20),
-              ZiggleRowButton(
-                title: Text(context.t.user.account.withdraw),
-                destructive: true,
-                showChevron: false,
-                onPressed: () => launchUrlString(Strings.withdrawalUrl),
-              ),
+              BlocBuilder<UserBloc, UserState>(builder: (context, state) {
+                if (state.user == null) {
+                  return ZiggleButton.cta(
+                    child: Text(context.t.user.account.login),
+                    onPressed: () =>
+                        context.read<AuthBloc>().add(const AuthEvent.login()),
+                  );
+                }
+                return Column(
+                  children: [
+                    ZiggleRowButton(
+                      title: Text(context.t.user.account.logout),
+                      destructive: true,
+                      showChevron: false,
+                      onPressed: () => context
+                          .read<AuthBloc>()
+                          .add(const AuthEvent.logout()),
+                    ),
+                    const SizedBox(height: 20),
+                    ZiggleRowButton(
+                      title: Text(context.t.user.account.withdraw),
+                      destructive: true,
+                      showChevron: false,
+                      onPressed: () => launchUrlString(Strings.withdrawalUrl),
+                    ),
+                  ],
+                );
+              }),
               _Title(title: context.t.user.setting.notification.title),
               FutureBuilder(
                 future:
