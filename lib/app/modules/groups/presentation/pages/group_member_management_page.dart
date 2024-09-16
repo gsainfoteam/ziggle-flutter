fc89a3eee69dcc2adbe0350d5734a77cd5c69f62
dawnfire05 <dawnfire05@naver.com>
2024-09-16 22:59:32 +0900

    feat: group member management page 1/2

--- lib/app/modules/groups/presentation/pages/group_member_management_page.dart
+++ lib/app/modules/groups/presentation/pages/group_member_management_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_app_bar.dart';
 
 class GroupMemberManagementPage extends StatelessWidget {
   const GroupMemberManagementPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: ZiggleAppBar.compact(
+        backLabel: '그룹 관리',
+        title: const Text('멤버 관리'),
+      ),
+    );
   }
 }
