122542a3f904f880e775d8985e34537dedab64b0
2paperstar <lee@paperst.ar>
2024-09-18 07:07:24 +0900

    fix: wrong import

--- lib/app/modules/core/data/data_sources/fcm_api.dart
+++ lib/app/modules/core/data/data_sources/fcm_api.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
-import 'package:retrofit/http.dart';
+import 'package:retrofit/retrofit.dart';
 
 part 'fcm_api.g.dart';
 
