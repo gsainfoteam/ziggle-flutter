a50dc8d542834a0b0007dd3697e8db8ff406e9dc
이신혁 <69188972+dawnfire05@users.noreply.github.com>
2024-09-17 20:29:19 +0900

    feat: add AgreementItem (#328)
    * feat: implement consent_item widget
    
    * feat: implement checkbox and color change
    
    * feat: agreement item
    
    * feat: added onchanged function

---
+++ lib/app/modules/notices/presentation/widgets/agreement_item.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_checkbox.dart';
+import 'package:ziggle/app/values/palette.dart';
+
+class AgreementItem extends StatelessWidget {
+  const AgreementItem({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.isChecked,
+    required this.onChanged,
+  });
+
+  final String title;
+  final String description;
+  final bool isChecked;
+  final void Function(bool) onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Durations.medium1,
+      decoration: ShapeDecoration(
+        color: isChecked ? Palette.primaryLight : Palette.grayLight,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 14),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ZiggleCheckbox(
+            onToggle: onChanged,
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    color: Palette.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: -0.45,
+                  ),
+                ),
+                Text(
+                  description,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
