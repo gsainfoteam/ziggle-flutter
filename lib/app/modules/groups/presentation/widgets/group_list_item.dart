1bed0b351263031f91fe78a734ff4912ad91022d
dawnfire05 <dawnfire05@naver.com>
2024-09-19 16:23:56 +0900

    fea: implement group list item

--- lib/app/modules/groups/presentation/widgets/group_list_item.dart
+++ lib/app/modules/groups/presentation/widgets/group_list_item.dart
@@ -1,25 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:ziggle/app/values/palette.dart';
+import 'package:ziggle/gen/assets.gen.dart';
 
 class GroupListItem extends StatelessWidget {
-  const GroupListItem({super.key});
+  const GroupListItem({
+    super.key,
+    required this.name,
+    this.isCertificated = false,
+    this.isGrouped = true,
+  });
+
+  final String name;
+  final bool isCertificated;
+  final bool isGrouped;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          height: 36,
-          width: 36,
-          decoration: ShapeDecoration(
-            color: Palette.primary,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(100),
+    return Container(
+      padding: isGrouped ? const EdgeInsets.all(10) : null,
+      decoration: isGrouped
+          ? ShapeDecoration(
+              color: Palette.grayLight,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            )
+          : null,
+      child: Row(
+        children: [
+          Container(
+            height: 36,
+            width: 36,
+            decoration: ShapeDecoration(
+              color: Palette.primary,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100),
+              ),
             ),
           ),
-        ),
-        const Text('그룹 이름', style: TextStyle(),)
-      ],
+          const SizedBox(width: 10),
+          const Text(
+            '그룹 이름',
+            style: TextStyle(),
+          ),
+          const SizedBox(width: 5),
+          if (isCertificated == true) Assets.icons.certificatedBadge.svg(),
+        ],
+      ),
     );
   }
 }
