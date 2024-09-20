79af485d379b17c9f9af9e6b6323ec7788a94151
이신혁 <69188972+dawnfire05@users.noreply.github.com>
2024-09-20 20:49:51 +0900

    feat: Member Card Widget (#391)
    * feat: editMode
    
    * fix: duplicated editMode
    
    * fix: enum return String
    
    * feat: used .map function
    
    * feat: added factory constructor

--- lib/app/modules/groups/presentation/widgets/group_member_card.dart
+++ lib/app/modules/groups/presentation/widgets/group_member_card.dart
@@ -4,29 +4,62 @@ import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_select.dar
 import 'package:ziggle/app/values/palette.dart';
 import 'package:ziggle/gen/strings.g.dart';
 
-enum GroupMemberRole { admin, manager, user }
+enum GroupMemberRole {
+  admin('Admin'),
+  manager('Manager'),
+  user('User');
+
+  final String label;
+
+  const GroupMemberRole(this.label);
+
+  String toLocalizedString(BuildContext context) {
+    switch (this) {
+      case GroupMemberRole.admin:
+        return context.t.common.memberCard.role.admin;
+      case GroupMemberRole.manager:
+        return context.t.common.memberCard.role.manager;
+      case GroupMemberRole.user:
+        return context.t.common.memberCard.role.user;
+    }
+  }
+}
 
 class GroupMemberCard extends StatelessWidget {
-  const GroupMemberCard({
+  final String name;
+  final String email;
+  final VoidCallback? onBanish;
+  final bool editMode;
+  final GroupMemberRole role;
+  final ValueChanged<GroupMemberRole?>? onChanged;
+
+  const GroupMemberCard.editMode({
     super.key,
     required this.name,
     required this.email,
-    this.onPressed,
     required this.role,
-  });
+    required this.onBanish,
+    required this.onChanged,
+  }) : editMode = true;
 
-  final String name;
-  final String email;
-  final VoidCallback? onPressed;
-  final GroupMemberRole? role;
+  const GroupMemberCard.viewMode({
+    super.key,
+    required this.name,
+    required this.email,
+    required this.role,
+  })  : editMode = false,
+        onBanish = null,
+        onChanged = null;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 15,
-      ),
+      padding: editMode
+          ? const EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 15,
+            )
+          : const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
       decoration: ShapeDecoration(
         color: Palette.grayLight,
         shape: RoundedRectangleBorder(
@@ -36,59 +69,72 @@ class GroupMemberCard extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            name,
-            style: const TextStyle(
-              color: Palette.black,
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          const SizedBox(height: 2),
-          Text(
-            email,
-            style: const TextStyle(
-              color: Palette.black,
-              fontSize: 14,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          IntrinsicHeight(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: ZiggleSelect(
-                    value: role,
-                    hintText: context.t.common.memberCard.role.role,
-                    entries: [
-                      ZiggleSelectEntry(
-                        value: GroupMemberRole.admin,
-                        label: context.t.common.memberCard.role.admin,
-                      ),
-                      ZiggleSelectEntry(
-                        value: GroupMemberRole.manager,
-                        label: context.t.common.memberCard.role.manager,
-                      ),
-                      ZiggleSelectEntry(
-                        value: GroupMemberRole.user,
-                        label: context.t.common.memberCard.role.user,
-                      )
-                    ],
-                    small: true,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: const TextStyle(
+                      color: Palette.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 2),
+                  Text(
+                    email,
+                    style: const TextStyle(
+                      color: Palette.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+              if (editMode != true)
+                Padding(
+                  padding: const EdgeInsets.only(right: 5),
+                  child: Text(
+                    role.toLocalizedString(context),
+                    style: const TextStyle(
+                      color: Palette.grayText,
+                      fontSize: 14,
+                    ),
                   ),
                 ),
-                const SizedBox(width: 10),
-                ZiggleButton.small(
-                  onPressed: onPressed,
-                  child: Text(context.t.common.memberCard.banish),
-                )
-              ],
-            ),
-          )
+            ],
+          ),
+          SizedBox(
+            height: editMode ? 10 : 0,
+          ),
+          if (editMode)
+            IntrinsicHeight(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: ZiggleSelect(
+                      onChanged: onChanged,
+                      value: role,
+                      hintText: context.t.common.memberCard.role.role,
+                      entries: GroupMemberRole.values
+                          .map((value) => ZiggleSelectEntry(
+                              value: value, label: value.label))
+                          .toList(),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  ZiggleButton.small(
+                    onPressed: onBanish,
+                    child: Text(context.t.common.memberCard.banish),
+                  )
+                ],
+              ),
+            )
         ],
       ),
     );
