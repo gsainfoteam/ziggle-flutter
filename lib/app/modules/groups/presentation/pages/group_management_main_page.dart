811b061e9096c44dfe83f9a48372fa33d9a5b179
이신혁 <69188972+dawnfire05@users.noreply.github.com>
2024-09-22 19:59:33 +0900

    feat:group management main page (#408)
    * feat: add no group image
    
    * feat: implement slang
    
    * feat: resolve requested changes
    
    * fix: remove unused 'back'
    
    * feat: use matrix to make svg black and white
    
    * feat: use greyscale svg

---
+++ lib/app/modules/groups/presentation/pages/group_management_main_page.dart
@@ -0,0 +1,98 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_app_bar.dart';
+import 'package:ziggle/app/values/palette.dart';
+import 'package:ziggle/gen/assets.gen.dart';
+import 'package:ziggle/gen/strings.g.dart';
+
+@RoutePage()
+class GroupManagementMainPage extends StatelessWidget {
+  const GroupManagementMainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: ZiggleAppBar.compact(
+        backLabel: context.t.user.myInfo,
+        title: Text(context.t.group.managementMain.header),
+        actions: [
+          GestureDetector(
+            child: Text(
+              context.t.group.managementMain.newGroup,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Palette.primary,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 25, 16, 10),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              context.t.group.managementMain.myGroup,
+              style: const TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Assets.images.bonfire.svg(),
+                    const SizedBox(height: 20),
+                    Text(
+                      context.t.group.managementMain.noGroup,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Palette.grayText,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              decoration: ShapeDecoration(
+                color: Palette.grayLight,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      child: Text(
+                        context.t.group.managementMain.contact,
+                        style: const TextStyle(
+                          color: Palette.grayText,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
