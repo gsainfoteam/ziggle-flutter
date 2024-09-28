023094915283e3c5531db707fc2964c2a8826901
dawnfire05 <dawnfire05@naver.com>
2024-09-28 12:18:30 +0900

    feat: add notion api

---
+++ lib/app/modules/groups/data/data_sources/remote/notion_api.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'notion_api.g.dart';
+
+@injectable
+@RestApi(baseUrl: 'notion/')
+abstract class NotionApi {
+  @factoryMethod
+  factory NotionApi(Dio dio) = _NotionApi;
+
+  @GET('{pageId}')
+  Future<Map<String, dynamic>> getGroups(@Path('pageId') int pageId);
+}
