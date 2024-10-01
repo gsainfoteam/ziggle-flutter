0e76322b62ce52bf5cee466eeea58fd4adf99941
2paperstar <lee@paperst.ar>
2024-10-01 16:51:05 +0900

    fix: remove current toast before show new one

--- lib/app/modules/common/presentation/extensions/toast.dart
+++ lib/app/modules/common/presentation/extensions/toast.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 extension BuildContextX on BuildContext {
   void showToast(String message) {
-    ScaffoldMessenger.of(this).showSnackBar(
-      SnackBar(
-        content: Text(message),
-      ),
-    );
+    ScaffoldMessenger.of(this)
+      ..clearSnackBars()
+      ..showSnackBar(
+        SnackBar(
+          content: Text(message),
+        ),
+      );
   }
 }
