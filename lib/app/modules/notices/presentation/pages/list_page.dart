15bf93b3ef3a98c78335363c1e41cb67aeb9ac7b
2paperstar <lee@paperst.ar>
2024-10-02 03:53:38 +0900

    feat: add pageview event for list page

--- lib/app/modules/notices/presentation/pages/list_page.dart
+++ lib/app/modules/notices/presentation/pages/list_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ziggle/app/di/locator.dart';
 import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_app_bar.dart';
+import 'package:ziggle/app/modules/core/data/models/analytics_event.dart';
+import 'package:ziggle/app/modules/core/domain/repositories/analytics_repository.dart';
 import 'package:ziggle/app/modules/notices/domain/enums/notice_type.dart';
 import 'package:ziggle/app/modules/notices/presentation/bloc/notice_list_bloc.dart';
 import 'package:ziggle/app/modules/notices/presentation/widgets/list_layout.dart';
@@ -10,10 +12,23 @@ import 'package:ziggle/app/values/palette.dart';
 import 'package:ziggle/gen/strings.g.dart';
 
 @RoutePage()
-class ListPage extends StatelessWidget {
+class ListPage extends StatefulWidget {
   const ListPage({super.key, required this.type});
 
   final NoticeType type;
+
+  @override
+  State<ListPage> createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage>
+    with AutoRouteAwareStateMixin<ListPage> {
+  @override
+  void didPush() =>
+      AnalyticsRepository.pageView(AnalyticsEvent.list(widget.type));
+  @override
+  void didPopNext() =>
+      AnalyticsRepository.pageView(AnalyticsEvent.list(widget.type));
 
   @override
   Widget build(BuildContext context) {
@@ -21,12 +36,13 @@ class ListPage extends StatelessWidget {
       backgroundColor: Palette.grayLight,
       appBar: ZiggleAppBar.compact(
         backLabel: context.t.notice.list,
-        title: Text(type.getName(context)),
+        title: Text(widget.type.getName(context)),
       ),
       body: BlocProvider(
-        create: (_) => sl<NoticeListBloc>()..add(NoticeListEvent.load(type)),
+        create: (_) =>
+            sl<NoticeListBloc>()..add(NoticeListEvent.load(widget.type)),
         child: ListLayout(
-          noticeType: type,
+          noticeType: widget.type,
         ),
       ),
     );
