80cdcaca50827f7b84424702fc957a33d9641c7c
2paperstar <lee@paperst.ar>
2024-10-02 04:32:26 +0900

    feat: add pageview event for notice edit preview page

--- lib/app/modules/notices/presentation/pages/notice_edit_preview_page.dart
+++ lib/app/modules/notices/presentation/pages/notice_edit_preview_page.dart
@@ -2,7 +2,9 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_app_bar.dart';
+import 'package:ziggle/app/modules/core/data/models/analytics_event.dart';
 import 'package:ziggle/app/modules/core/domain/enums/page_source.dart';
+import 'package:ziggle/app/modules/core/domain/repositories/analytics_repository.dart';
 import 'package:ziggle/app/modules/notices/domain/entities/notice_entity.dart';
 import 'package:ziggle/app/modules/notices/presentation/bloc/notice_bloc.dart';
 import 'package:ziggle/app/modules/notices/presentation/bloc/notice_write_bloc.dart';
@@ -10,8 +12,23 @@ import 'package:ziggle/app/modules/notices/presentation/widgets/notice_renderer.
 import 'package:ziggle/gen/strings.g.dart';
 
 @RoutePage()
-class NoticeEditPreviewPage extends StatelessWidget {
+class NoticeEditPreviewPage extends StatefulWidget {
   const NoticeEditPreviewPage({super.key});
+
+  @override
+  State<NoticeEditPreviewPage> createState() => _NoticeEditPreviewPageState();
+}
+
+class _NoticeEditPreviewPageState extends State<NoticeEditPreviewPage>
+    with AutoRouteAwareStateMixin<NoticeEditPreviewPage> {
+  @override
+  void didPush() =>
+      AnalyticsRepository.pageView(AnalyticsEvent.noticeEditPreview(
+          context.read<NoticeBloc>().state.entity!.id));
+  @override
+  void didPopNext() =>
+      AnalyticsRepository.pageView(AnalyticsEvent.noticeEditPreview(
+          context.read<NoticeBloc>().state.entity!.id));
 
   @override
   Widget build(BuildContext context) {
