99e050f954096093e02a66bca312a0db7751b2b0
2paperstar <lee@paperst.ar>
2024-10-03 23:51:15 +0900

    feat: link firebase analytics repository to MultipleAnalyticsRepository

--- lib/app/modules/core/data/repositories/multiple_analytics_repository.dart
+++ lib/app/modules/core/data/repositories/multiple_analytics_repository.dart
@@ -1,5 +1,6 @@
 import 'package:injectable/injectable.dart';
 import 'package:ziggle/app/modules/core/data/models/analytics_event.dart';
+import 'package:ziggle/app/modules/core/data/repositories/firebase_analytics_repository.dart';
 import 'package:ziggle/app/modules/core/domain/enums/event_type.dart';
 import 'package:ziggle/app/modules/core/domain/repositories/analytics_repository.dart';
 import 'package:ziggle/app/modules/user/domain/entities/user_entity.dart';
@@ -7,21 +8,31 @@ import 'package:ziggle/app/modules/user/domain/entities/user_entity.dart';
 @Singleton(as: AnalyticsRepository)
 @prod
 class MultipleAnalyticsRepository implements AnalyticsRepository {
+  final FirebaseAnalyticsRepository _firebaseAnalyticsRepository;
+  late final _repositories = [
+    _firebaseAnalyticsRepository,
+  ];
+
+  MultipleAnalyticsRepository(this._firebaseAnalyticsRepository);
+
   @override
   logChangeUser(UserEntity? user) {
-    // TODO: implement logChangeUser
-    throw UnimplementedError();
+    for (final repository in _repositories) {
+      repository.logChangeUser(user);
+    }
   }
 
   @override
   logEvent(EventType type, AnalyticsEvent event) {
-    // TODO: implement logEvent
-    throw UnimplementedError();
+    for (final repository in _repositories) {
+      repository.logEvent(type, event);
+    }
   }
 
   @override
   logScreen(String screenName) {
-    // TODO: implement logScreen
-    throw UnimplementedError();
+    for (final repository in _repositories) {
+      repository.logScreen(screenName);
+    }
   }
 }
