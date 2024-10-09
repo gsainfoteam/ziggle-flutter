d3e312057df89ec2e9ab64305f0b5674272fc2c7
dawnfire05 <dawnfire05@naver.com>
2024-10-10 04:37:16 +0900

    feat: divide api channel

---
+++ lib/app/modules/core/domain/enums/groups_api_channel.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+
+enum GroupsApiChannel {
+  staging('https://api.groups.gistory.me/'),
+  production('https://api.groups.gistory.me/');
+
+  final String baseUrl;
+  const GroupsApiChannel(this.baseUrl);
+
+  factory GroupsApiChannel.byMode() => kDebugMode ? staging : production;
+  GroupsApiChannel get oppose => this == staging ? production : staging;
+}
