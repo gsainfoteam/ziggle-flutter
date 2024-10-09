82933c7c58a37132df3bfd7e4e3467eca14482b5
dawnfire05 <dawnfire05@naver.com>
2024-10-10 05:49:02 +0900

    feat: use seperated interceptor

--- lib/app/modules/core/data/dio/groups_dio.dart
+++ lib/app/modules/core/data/dio/groups_dio.dart
@@ -3,11 +3,12 @@ import 'package:dio/src/dio/dio_for_native.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:injectable/injectable.dart';
 import 'package:ziggle/app/modules/user/data/data_sources/remote/authorize_interceptor.dart';
+import 'package:ziggle/app/modules/user/data/data_sources/remote/groups_authorize_interceptor.dart';
 
 @singleton
 class GroupsDio extends DioForNative {
-  GroupsDio(
-      AuthorizeInterceptor authorizeInterceptor, CookieManager cookieManager) {
+  GroupsDio(GroupsAuthorizeInterceptor authorizeInterceptor,
+      CookieManager cookieManager) {
     interceptors.addAll([authorizeInterceptor, cookieManager]);
   }
 }
