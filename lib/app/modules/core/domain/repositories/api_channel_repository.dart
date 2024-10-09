6288cdfe02005cbb3887cf76207e1c5161c18936
dawnfire05 <dawnfire05@naver.com>
2024-10-10 06:17:12 +0900

    refactor : api channel repositoires

--- lib/app/modules/core/domain/repositories/api_channel_repository.dart
+++ lib/app/modules/core/domain/repositories/api_channel_repository.dart
@@ -1,11 +1,9 @@
-import 'package:ziggle/app/modules/core/domain/enums/groups_api_channel.dart';
-import 'package:ziggle/app/modules/core/domain/enums/ziggle_api_channel.dart';
+import 'package:ziggle/app/modules/core/domain/enums/api_channel.dart';
 
 abstract class ApiChannelRepository {
-  //void setChannel(ApiChannel channel);
-  ZiggleApiChannel toggleZiggleChannel();
-  GroupsApiChannel toggleGroupsChannel();
-  // String get baseUrl;
-  Stream<ZiggleApiChannel> get ziggleChannel;
-  Stream<GroupsApiChannel> get groupsChannel;
+  void setChannel(ApiChannel channel);
+  ApiChannel toggleChannel();
+  String get ziggleBaseUrl;
+  String get groupsBaseUrl;
+  Stream<ApiChannel> get channel;
 }
