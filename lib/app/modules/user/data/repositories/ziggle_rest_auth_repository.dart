883048af6f60ecbe39b7a1e62f37f0284318888c
dawnfire05 <dawnfire05@naver.com>
2024-10-12 06:56:30 +0900

    feat: rest auth repositories are singelton

--- lib/app/modules/user/data/repositories/ziggle_rest_auth_repository.dart
+++ lib/app/modules/user/data/repositories/ziggle_rest_auth_repository.dart
@@ -1,9 +1,11 @@
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+import 'package:injectable/injectable.dart';
 import 'package:ziggle/app/modules/user/data/data_sources/remote/user_api.dart';
 import 'package:ziggle/app/modules/user/data/repositories/rest_auth_repository.dart';
 import 'package:ziggle/app/modules/user/data/repositories/ziggle_flutter_secure_storage_token_repository.dart';
 import 'package:ziggle/app/modules/user/data/repositories/ziggle_web_auth_2_oauth_repository.dart';
 
+@Singleton()
 class ZiggleRestAuthRepository extends RestAuthRepository {
   ZiggleRestAuthRepository(
       UserApi api,
