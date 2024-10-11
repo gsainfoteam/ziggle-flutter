88ced3e89f15e02d8695a29cb1b3d51da23186a7
dawnfire05 <dawnfire05@naver.com>
2024-10-12 06:53:52 +0900

    feat: group auth logic

--- lib/app/modules/user/data/data_sources/remote/groups_authorize_interceptor.dart
+++ lib/app/modules/user/data/data_sources/remote/groups_authorize_interceptor.dart
@@ -1,8 +1,9 @@
 import 'package:injectable/injectable.dart';
 import 'package:ziggle/app/modules/user/data/data_sources/remote/authorize_interceptor.dart';
-import 'package:ziggle/app/modules/user/data/repositories/groups_token_repository.dart';
+import 'package:ziggle/app/modules/user/data/repositories/groups_flutter_secure_storage_token_repository.dart';
 
 @singleton
 class GroupsAuthorizeInterceptor extends AuthorizeInterceptor {
-  GroupsAuthorizeInterceptor(GroupsTokenRepository super.repository);
+  GroupsAuthorizeInterceptor(
+      GroupsFlutterSecureStorageTokenRepository super.repository);
 }
