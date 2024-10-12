b1ef615be1f210bb88dd10bac45e751cf5bf49a0
dawnfire05 <dawnfire05@naver.com>
2024-10-13 04:44:27 +0900

    feat: add logger in GroupsDio

--- lib/app/modules/core/data/dio/groups_dio.dart
+++ lib/app/modules/core/data/dio/groups_dio.dart
@@ -1,4 +1,5 @@
 // ignore: implementation_imports
+import 'package:dio/dio.dart';
 import 'package:dio/src/dio/dio_for_native.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:injectable/injectable.dart';
@@ -9,5 +10,11 @@ class GroupsDio extends DioForNative {
   GroupsDio(GroupsAuthorizeInterceptor authorizeInterceptor,
       CookieManager cookieManager) {
     interceptors.addAll([authorizeInterceptor, cookieManager]);
+    interceptors.add(LogInterceptor(
+      request: true,
+      requestBody: true,
+      responseBody: true,
+      responseHeader: false,
+    ));
   }
 }
