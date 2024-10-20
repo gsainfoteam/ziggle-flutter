78101c1891583c7cf45dd5fdbc3e9d14d389dc28
2paperstar <lee@paperst.ar>
2024-10-20 10:18:07 +0900

    fix: infinite scroll primary scroll issue

--- lib/app/modules/notices/presentation/widgets/infinite_scroll.dart
+++ lib/app/modules/notices/presentation/widgets/infinite_scroll.dart
@@ -1,42 +1,64 @@
 import 'package:flutter/material.dart';
 
-class InfiniteScroll extends StatefulWidget {
+class InfiniteScroll extends StatelessWidget {
   const InfiniteScroll({
     super.key,
     required this.onLoadMore,
     required this.slivers,
     this.threshold = 200,
+    this.controller,
   });
 
   final VoidCallback onLoadMore;
   final List<Widget> slivers;
   final int threshold;
+  final ScrollController? controller;
 
   @override
-  State<InfiniteScroll> createState() => _InfiniteScrollState();
+  Widget build(BuildContext context) {
+    return _Inner(
+      controller: controller ?? PrimaryScrollController.of(context),
+      onLoadMore: onLoadMore,
+      slivers: slivers,
+      threshold: threshold,
+    );
+  }
 }
 
-class _InfiniteScrollState extends State<InfiniteScroll> {
-  final _scrollController = ScrollController();
+class _Inner extends StatefulWidget {
+  const _Inner({
+    required this.onLoadMore,
+    required this.slivers,
+    this.threshold = 200,
+    required this.controller,
+  });
 
+  final ScrollController controller;
+  final VoidCallback onLoadMore;
+  final List<Widget> slivers;
+  final int threshold;
+
+  @override
+  State<_Inner> createState() => _InnerState();
+}
+
+class _InnerState extends State<_Inner> {
   @override
   void initState() {
     super.initState();
-    _scrollController.addListener(_onScroll);
+    widget.controller.addListener(_onScroll);
   }
 
   @override
   void dispose() {
-    _scrollController
-      ..removeListener(_onScroll)
-      ..dispose();
+    widget.controller.removeListener(_onScroll);
     super.dispose();
   }
 
   bool get _isBottom {
-    if (!_scrollController.hasClients) return false;
-    final maxScroll = _scrollController.position.maxScrollExtent;
-    final currentScroll = _scrollController.offset;
+    if (!widget.controller.hasClients) return false;
+    final maxScroll = widget.controller.position.maxScrollExtent;
+    final currentScroll = widget.controller.offset;
     return currentScroll >= maxScroll - widget.threshold;
   }
 
@@ -47,7 +69,7 @@ class _InfiniteScrollState extends State<InfiniteScroll> {
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
-      controller: _scrollController,
+      controller: widget.controller,
       slivers: widget.slivers,
     );
   }
