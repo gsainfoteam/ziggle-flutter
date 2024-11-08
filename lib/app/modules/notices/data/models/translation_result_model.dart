f16d79c3c57ade3dfb4ff72729a295c151d20c7a
2paperstar <lee@paperst.ar>
2024-11-08 13:23:16 +0900

    fix: ignore lang field of translation response

--- lib/app/modules/notices/data/models/translation_result_model.dart
+++ lib/app/modules/notices/data/models/translation_result_model.dart
@@ -1,5 +1,4 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:ziggle/app/modules/core/domain/enums/language.dart';
 
 part 'translation_result_model.freezed.dart';
 part 'translation_result_model.g.dart';
@@ -8,7 +7,6 @@ part 'translation_result_model.g.dart';
 class TranslationResultModel with _$TranslationResultModel {
   const factory TranslationResultModel({
     required String text,
-    required Language lang,
   }) = _TranslationResultModel;
 
   factory TranslationResultModel.fromJson(Map<String, dynamic> json) =>
