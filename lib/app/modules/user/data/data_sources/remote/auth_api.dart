a95f002842a23588877b6d72f5cbea929a24c0f4
dawnfire05 <dawnfire05@naver.com>
2024-11-11 16:27:05 +0900

    feat: move groups related auth logic

--- lib/app/modules/user/data/data_sources/remote/auth_api.dart
+++ lib/app/modules/user/data/data_sources/remote/auth_api.dart
@@ -2,9 +2,9 @@ import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:ziggle/app/modules/core/data/dio/groups_dio.dart';
-import 'package:ziggle/app/modules/groups/data/data_sources/models/auth_token_model.dart';
 import 'package:ziggle/app/modules/groups/data/data_sources/models/group_user_model.dart';
 import 'package:ziggle/app/modules/user/data/data_sources/remote/base_auth_api.dart';
+import 'package:ziggle/app/modules/user/data/models/auth_token_model.dart';
 import 'package:ziggle/app/values/strings.dart';
 
 part 'auth_api.g.dart';
