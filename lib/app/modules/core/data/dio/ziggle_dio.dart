3c80ac548750a781baeaf4d3b6ac35de97a1b022
dawnfire05 <dawnfire05@naver.com>
2024-11-12 21:16:49 +0900

    feat: add implemented cookie manager to dios

--- lib/app/modules/core/data/dio/ziggle_dio.dart
+++ lib/app/modules/core/data/dio/ziggle_dio.dart
@@ -1,13 +1,13 @@
 // ignore: implementation_imports
 import 'package:dio/src/dio/dio_for_native.dart';
-import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:injectable/injectable.dart';
 import 'package:ziggle/app/modules/user/data/data_sources/remote/ziggle_authorize_interceptor.dart';
+import 'package:ziggle/app/modules/user/data/data_sources/remote/ziggle_cookie_manager.dart';
 
 @singleton
 class ZiggleDio extends DioForNative {
   ZiggleDio(ZiggleAuthorizeInterceptor authorizeInterceptor,
-      CookieManager cookieManager) {
+      ZiggleCookieManager cookieManager) {
     interceptors.addAll([authorizeInterceptor, cookieManager]);
   }
 }
