4b620da284ee884038f8912f61f7089b8789b149
dawnfire05 <dawnfire05@naver.com>
2024-12-29 23:49:14 +0900

    fix: group model

--- lib/app/modules/groups/data/data_sources/models/group_user_model.dart
+++ lib/app/modules/groups/data/data_sources/models/group_user_model.dart
@@ -1,17 +1,16 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:ziggle/app/modules/user/domain/entities/user_entity.dart';
 
 part 'group_user_model.freezed.dart';
 part 'group_user_model.g.dart';
 
 @freezed
-class GroupUserModel with _$GroupUserModel implements UserEntity {
+class GroupUserModel with _$GroupUserModel {
   const GroupUserModel._();
   const factory GroupUserModel({
-    required String email,
-    required String name,
-    @JsonKey(name: 'studentNumber') required String studentId,
     required String uuid,
+    required String name,
+    required String email,
+    required DateTime createdAt,
   }) = _UserModel;
 
   factory GroupUserModel.fromJson(Map<String, dynamic> json) =>
