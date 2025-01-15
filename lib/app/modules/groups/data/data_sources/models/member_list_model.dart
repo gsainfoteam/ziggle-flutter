7b31cf6b20822854f0a5640112aa9d553a0f9d47
dawnfire05 <dawnfire05@naver.com>
2025-01-15 18:02:29 +0900

    fix: name of the group list model

---
+++ lib/app/modules/groups/data/data_sources/models/member_list_model.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:ziggle/app/modules/groups/data/data_sources/models/member_model.dart';
+import 'package:ziggle/app/modules/groups/domain/entities/member_list_entity.dart';
+
+part 'member_list_model.freezed.dart';
+part 'member_list_model.g.dart';
+
+@freezed
+class MemberListModel with _$MemberListModel implements MemberListEntity {
+  factory MemberListModel(List<MemberModel> list) = _MemberListModel;
+
+  factory MemberListModel.fromJson(Map<String, dynamic> json) =>
+      _$MemberListModelFromJson(json);
+}
