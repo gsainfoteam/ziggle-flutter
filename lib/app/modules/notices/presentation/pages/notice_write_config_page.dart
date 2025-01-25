0a8379427c693ac9b66d2c15d09a575d097325e4
andongyeon <a56634368@gmail.com>
2025-01-25 19:57:41 +0900

    feat: add change account widget on notice write page

--- lib/app/modules/notices/presentation/pages/notice_write_config_page.dart
+++ lib/app/modules/notices/presentation/pages/notice_write_config_page.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
+import 'package:ziggle/app/di/locator.dart';
 import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_app_bar.dart';
 import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_bottom_sheet.dart';
 import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_button.dart';
@@ -12,12 +13,15 @@ import 'package:ziggle/app/modules/core/domain/enums/page_source.dart';
 import 'package:ziggle/app/modules/core/domain/repositories/analytics_repository.dart';
 import 'package:ziggle/app/modules/notices/domain/enums/notice_type.dart';
 import 'package:ziggle/app/modules/notices/presentation/bloc/notice_write_bloc.dart';
+import 'package:ziggle/app/modules/notices/presentation/widgets/account_selector.dart';
 import 'package:ziggle/app/modules/notices/presentation/widgets/deadline_selector.dart';
 import 'package:ziggle/app/modules/notices/presentation/widgets/tag.dart';
+import 'package:ziggle/app/modules/user/presentation/bloc/user_bloc.dart';
 import 'package:ziggle/app/router.gr.dart';
 import 'package:ziggle/app/values/palette.dart';
 import 'package:ziggle/gen/assets.gen.dart';
 import 'package:ziggle/gen/strings.g.dart';
+import 'package:ziggle/app/modules/groups/presentation/blocs/group_management_main_bloc.dart';
 
 @RoutePage()
 class NoticeWriteConfigPage extends StatefulWidget {
@@ -95,6 +99,8 @@ class _NoticeWriteConfigPageState extends State<NoticeWriteConfigPage>
             padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 16),
             child: Column(
               children: [
+                _buildChangeAccount(),
+                const SizedBox(height: 25),
                 _buildDeadline(),
                 const SizedBox(height: 25),
                 _buildCategory(),
@@ -117,6 +123,69 @@ class _NoticeWriteConfigPageState extends State<NoticeWriteConfigPage>
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildChangeAccount() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: const BoxDecoration(
+        color: Palette.grayLight,
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            children: [
+              const SizedBox(width: 5),
+              Assets.images.defaultProfile.image(width: 40),
+              const SizedBox(width: 10),
+              BlocBuilder<UserBloc, UserState>(
+                builder: (context, userState) {
+                  return Row(children: [
+                    Text(
+                      userState.user!.name,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Palette.black,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ]);
+                },
+              ),
+              const Spacer(),
+              ZigglePressable(
+                onPressed: () async {
+                  final userAccount = await ZiggleBottomSheet.show<DateTime>(
+                    context: context,
+                    title: context.t.notice.write.changeAccount,
+                    builder: (context) => AccountSelector(),
+                  );
+                },
+                child: Row(
+                  children: [
+                    Text(
+                      context.t.notice.write.changeAccount,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Palette.grayText,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    Assets.icons.arrowRight.svg(
+                      width: 20,
+                      height: 20,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
