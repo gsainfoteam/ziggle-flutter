ed5c49e0bc0821846a360fe2411f7b90e31fa6bc
andongyeon <a56634368@gmail.com>
2025-02-06 20:36:14 +0900

    feat: add ziggle group api

---
+++ lib/app/modules/notices/data/data_sources/remote/ziggle_group_api.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:ziggle/app/modules/core/data/dio/ziggle_dio.dart';
+part 'ziggle_group_api.g.dart';
+
+@injectable
+@RestApi(baseUrl: 'group/')
+abstract class ZiggleGroupApi {
+  @factoryMethod
+  factory ZiggleGroupApi(ZiggleDio dio) = _ZiggleGroupApi;
+
+  @POST('token')
+  Future<String> getGroupToken();
+}
