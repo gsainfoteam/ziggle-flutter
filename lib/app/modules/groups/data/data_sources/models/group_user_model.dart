1a5ad29e8318fa9d6d22adc3ddf580cbb81099da
dawnfire05 <dawnfire05@naver.com>
2025-02-08 21:54:38 +0900

    feat: group user model now implements group user entity

--- lib/app/modules/groups/data/data_sources/models/group_user_model.dart
+++ lib/app/modules/groups/data/data_sources/models/group_user_model.dart
@@ -1,10 +1,11 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:ziggle/app/modules/groups/domain/entities/group_user_entity.dart';
 
 part 'group_user_model.freezed.dart';
 part 'group_user_model.g.dart';
 
 @freezed
-class GroupUserModel with _$GroupUserModel {
+class GroupUserModel with _$GroupUserModel implements GroupUserEntity {
   const GroupUserModel._();
   const factory GroupUserModel({
     required String uuid,
---
+++ lib/app/modules/groups/domain/entities/group_user_entity.dart
@@ -0,0 +1,13 @@
+class GroupUserEntity {
+  final String uuid;
+  final String name;
+  final String email;
+  final DateTime createdAt;
+
+  GroupUserEntity({
+    required this.uuid,
+    required this.name,
+    required this.email,
+    required this.createdAt,
+  });
+}
