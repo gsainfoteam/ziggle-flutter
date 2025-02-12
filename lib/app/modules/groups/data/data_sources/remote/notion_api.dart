222862cf9c0a0896003571c1337116e3bb2ad328
dawnfire05 <dawnfire05@naver.com>
2025-02-12 23:15:38 +0900

    fix: remove unused model

--- lib/app/modules/groups/data/data_sources/remote/notion_api.dart
+++ lib/app/modules/groups/data/data_sources/remote/notion_api.dart
@@ -2,7 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:ziggle/app/modules/core/data/dio/groups_dio.dart';
-import 'package:ziggle/app/modules/groups/data/data_sources/models/notion_response_model.dart';
 
 part 'notion_api.g.dart';
 
@@ -13,5 +12,5 @@ abstract class NotionApi {
   factory NotionApi(GroupsDio dio) = _NotionApi;
 
   @GET('{pageId}')
-  Future<NotionResponseModel> getGroups(@Path('pageId') String pageId);
+  Future<String> getGroups(@Path('pageId') String pageId);
 }
