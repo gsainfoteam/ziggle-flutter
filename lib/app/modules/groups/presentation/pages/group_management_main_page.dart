2fde4b0ec0ec3a02c1f9477461239070e59cec57
dawnfire05 <dawnfire05@naver.com>
2025-02-23 22:26:07 +0900

    feat: group management main page with profile image

--- lib/app/modules/groups/presentation/pages/group_management_main_page.dart
+++ lib/app/modules/groups/presentation/pages/group_management_main_page.dart
@@ -93,6 +93,15 @@ class _Layout extends StatelessWidget {
                             }
                             return GroupListItem(
                               name: groups.list[index].name,
+                              profileImage:
+                                  state.groups!.list[index].profileImageUrl !=
+                                          null
+                                      ? Image.network(
+                                          state.groups!.list[index]
+                                              .profileImageUrl!,
+                                          fit: BoxFit.fill,
+                                        )
+                                      : null,
                               onPressed: () {
                                 context.router.push(
                                   GroupManagementShellRoute(
