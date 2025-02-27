eec6200b08a4d9886d4ac969cef3eb4b7da845b6
dawnfire05 <dawnfire05@naver.com>
2025-02-28 01:43:16 +0900

    feat: don't include field when the value is null

--- lib/app/modules/groups/data/data_sources/models/modify_group_model.dart
+++ lib/app/modules/groups/data/data_sources/models/modify_group_model.dart
@@ -6,21 +6,11 @@ part 'modify_group_model.g.dart';
 @freezed
 class ModifyGroupModel with _$ModifyGroupModel {
   factory ModifyGroupModel({
-    String? name,
-    String? description,
-    String? notionPageId,
+    @JsonKey(includeIfNull: false) String? name,
+    @JsonKey(includeIfNull: false) String? description,
+    @JsonKey(includeIfNull: false) String? notionPageId,
   }) = _ModifyGroupModel;
 
   factory ModifyGroupModel.fromJson(Map<String, dynamic> json) =>
       _$ModifyGroupModelFromJson(json);
-}
-
-extension ModifyGroupModelX on ModifyGroupModel {
-  Map<String, dynamic> toJson() {
-    final json = <String, dynamic>{};
-    if (name != null) json['name'] = name;
-    if (description != null) json['description'] = description;
-    if (notionPageId != null) json['notionPageId'] = notionPageId;
-    return json;
-  }
 }
