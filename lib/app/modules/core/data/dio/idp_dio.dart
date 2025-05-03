63fcaaeb7c412904659931482552174092bb11a5
2paperstar <lee@paperst.ar>
2025-05-03 23:26:42 +0900

    feat: add idp api base url

--- lib/app/modules/core/data/dio/idp_dio.dart
+++ lib/app/modules/core/data/dio/idp_dio.dart
@@ -1,7 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
 import 'package:injectable/injectable.dart';
+import 'package:ziggle/app/values/strings.dart';
 
 @singleton
 class IdPDio extends DioForNative {
-  IdPDio();
+  IdPDio() : super(BaseOptions(baseUrl: Strings.idpApiBaseUrl));
 }
