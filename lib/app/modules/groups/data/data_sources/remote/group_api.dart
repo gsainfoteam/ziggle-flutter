1da6a96b23b6d6b4177495a42dcd43f8398b7714
andongyeon <a56634368@gmail.com>
2025-09-07 05:08:09 +0900

    feat: add change president api

--- lib/app/modules/groups/data/data_sources/remote/group_api.dart
+++ lib/app/modules/groups/data/data_sources/remote/group_api.dart
@@ -13,6 +13,7 @@ import 'package:ziggle/app/modules/groups/data/data_sources/models/member_list_m
 import 'package:ziggle/app/modules/groups/data/data_sources/models/modify_group_model.dart';
 import 'package:ziggle/app/modules/groups/data/data_sources/models/role_list_model.dart';
 import 'package:ziggle/app/modules/groups/data/data_sources/models/role_model.dart';
+import 'package:ziggle/app/modules/groups/data/data_sources/models/update_president_model.dart';
 import 'package:ziggle/app/modules/groups/data/data_sources/models/update_role_model.dart';
 
 part 'group_api.g.dart';
@@ -104,7 +105,7 @@ abstract class GroupApi {
   @PATCH('{uuid}/president')
   Future<void> updatePresident(
     @Path('uuid') String uuid,
-    @Body() Map<String, String> newPresidentUuid,
+    @Body() UpdatePresidentModel newPresidentUuid,
   );
 
   @GET('{groupUuid}/role')
