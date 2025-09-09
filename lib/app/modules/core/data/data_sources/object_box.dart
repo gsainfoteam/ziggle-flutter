9db41c6eb9ff8d4820013948a9652a9419bc3995
dawnfire05 <dawnfire05@naver.com>
2025-09-10 04:25:05 +0900

    feat: create object box setup class

---
+++ lib/app/modules/core/data/data_sources/object_box.dart
@@ -0,0 +1,16 @@
+import 'package:objectbox/objectbox.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'objectbox.g.dart';
+
+class ObjectBox {
+  late final Store store;
+
+  ObjectBox._create(this.store);
+
+  static Future<ObjectBox> create() async {
+    final docsDir = await getApplicationDocumentsDirectory();
+    final store = await openStore(directory: p.join(docsDir.path, "ziggle"));
+    return ObjectBox._create(store);
+  }
+}
