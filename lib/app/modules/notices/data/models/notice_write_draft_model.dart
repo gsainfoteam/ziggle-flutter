060a3c93b8a9ab453868c84968adbf2565aa7bbf
dawnfire05 <dawnfire05@naver.com>
2025-09-10 17:06:44 +0900

    refactor: simplify notice write draft model

--- lib/app/modules/notices/data/models/notice_write_draft_model.dart
+++ lib/app/modules/notices/data/models/notice_write_draft_model.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:objectbox/objectbox.dart';
 import 'package:ziggle/app/modules/core/domain/enums/language.dart';
@@ -12,46 +11,14 @@ sealed class NoticeWriteDraftModel with _$NoticeWriteDraftModel {
   @Entity(realClass: NoticeWriteDraftModel)
   factory NoticeWriteDraftModel({
     @Id(assignable: true) @Default(0) int id,
-    @Default('{}') String titlesJson,
-    @Default('{}') String bodiesJson,
-    @Default(-1) int typeIndex,
-    @Default('') String tagsJson,
+    @Default({}) Map<Language, String> titles,
+    @Default({}) Map<Language, String> bodies,
+    NoticeType? type,
+    @Default([]) List<String> tags,
     @Property(type: PropertyType.date) DateTime? deadline,
   }) = _NoticeWriteDraftModel;
 
   const NoticeWriteDraftModel._();
-
-  Map<Language, String> get titles {
-    try {
-      final Map<String, dynamic> json = jsonDecode(titlesJson);
-      return json.map(
-        (key, value) => MapEntry(Language.values.byName(key), value.toString()),
-      );
-    } catch (e) {
-      return {};
-    }
-  }
-
-  Map<Language, String> get bodies {
-    try {
-      final Map<String, dynamic> json = jsonDecode(bodiesJson);
-      return json.map(
-        (key, value) => MapEntry(Language.values.byName(key), value.toString()),
-      );
-    } catch (e) {
-      return {};
-    }
-  }
-
-  NoticeType? get type => typeIndex >= 0 ? NoticeType.values[typeIndex] : null;
-
-  List<String> get tags {
-    try {
-      return tagsJson.isEmpty ? [] : tagsJson.split(',');
-    } catch (e) {
-      return [];
-    }
-  }
 
   NoticeWriteDraftEntity toEntity() => NoticeWriteDraftEntity(
     titles: titles,
@@ -63,14 +30,10 @@ sealed class NoticeWriteDraftModel with _$NoticeWriteDraftModel {
 
   factory NoticeWriteDraftModel.fromEntity(NoticeWriteDraftEntity entity) =>
       NoticeWriteDraftModel(
-        titlesJson: jsonEncode(
-          entity.titles.map((key, value) => MapEntry(key.name, value)),
-        ),
-        bodiesJson: jsonEncode(
-          entity.bodies.map((key, value) => MapEntry(key.name, value)),
-        ),
-        typeIndex: entity.type?.index ?? -1,
-        tagsJson: entity.tags.join(','),
+        titles: entity.titles,
+        bodies: entity.bodies,
+        type: entity.type,
+        tags: entity.tags,
         deadline: entity.deadline,
       );
 }
