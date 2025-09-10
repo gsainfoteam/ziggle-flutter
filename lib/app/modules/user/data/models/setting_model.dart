d2f8a9690572cc29624ff54edc54fe41dbba9642
dawnfire05 <dawnfire05@naver.com>
2025-09-10 13:13:07 +0900

    chore: SettingModel 클래스의 생성자 및 copyWith 메서드의 코드 스타일 개선

--- lib/app/modules/user/data/models/setting_model.dart
+++ lib/app/modules/user/data/models/setting_model.dart
@@ -1,27 +1,25 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:objectbox/objectbox.dart';
 import 'package:ziggle/gen/strings.g.dart';
 
 @Entity()
-@freezed
 class SettingModel {
   @Id()
   int id;
   String language;
   bool developerOption;
 
-  SettingModel(
-      {this.id = 0, required this.language, required this.developerOption});
+  SettingModel({
+    this.id = 0,
+    required this.language,
+    required this.developerOption,
+  });
 
   factory SettingModel.init() => SettingModel(
-        language: AppLocaleUtils.findDeviceLocale().name,
-        developerOption: false,
-      );
+    language: AppLocaleUtils.findDeviceLocale().name,
+    developerOption: false,
+  );
 
-  SettingModel copyWith({
-    String? language,
-    bool? developerOption,
-  }) =>
+  SettingModel copyWith({String? language, bool? developerOption}) =>
       SettingModel(
         id: id,
         language: language ?? this.language,
