2b1ce350050c3e67d83736f89394e6780937aefa
2paperstar <lee@paperst.ar>
2025-09-21 11:30:31 +0900

    fix: wait until auth loaded in splash screen

--- lib/app/modules/splash/presentation/pages/splash_page.dart
+++ lib/app/modules/splash/presentation/pages/splash_page.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ziggle/app/modules/core/presentation/bloc/link_bloc.dart';
+import 'package:ziggle/app/modules/user/presentation/bloc/auth_bloc.dart';
 import 'package:ziggle/app/router.gr.dart';
 import 'package:ziggle/app/values/palette.dart';
 import 'package:ziggle/gen/assets.gen.dart';
@@ -20,25 +21,25 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      Future.delayed(
-        widget.delay ? const Duration(seconds: 1) : Duration.zero,
-        () {
-          if (!mounted) return;
-          final linkData = context.read<LinkBloc>().state.whenOrNull(
-            loaded: (link) => link,
-          );
-          if (linkData != null) {
-            try {
-              context.router
-                ..replaceAll([const FeedRoute()])
-                ..replacePath(linkData);
-              return;
-            } catch (_) {}
-          }
-          context.router.replaceAll([const FeedRoute()]);
-        },
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      final authBloc = context.read<AuthBloc>();
+      await Future.wait([
+        if (widget.delay) Future.delayed(const Duration(seconds: 1)),
+        authBloc.stream.firstWhere((s) => !s.isLoading),
+      ]);
+      if (!mounted) return;
+      final linkData = context.read<LinkBloc>().state.whenOrNull(
+        loaded: (link) => link,
       );
+      if (linkData != null) {
+        try {
+          context.router
+            ..replaceAll([const FeedRoute()])
+            ..replacePath(linkData);
+          return;
+        } catch (_) {}
+      }
+      context.router.replaceAll([const FeedRoute()]);
     });
   }
 
