8c461ba6798e091d88eb9a2e9d9d0dbbe27c8b77
2paperstar <lee@paperst.ar>
2025-09-21 22:33:44 +0900

    feat: add consent page

--- lib/app/modules/auth/presentation/pages/consent_page.dart
+++ lib/app/modules/auth/presentation/pages/consent_page.dart
@@ -1,5 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_app_bar.dart';
+import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_button.dart';
+import 'package:ziggle/app/modules/common/presentation/widgets/ziggle_pressable.dart';
+import 'package:ziggle/app/values/palette.dart';
+import 'package:ziggle/app/values/strings.dart';
+import 'package:ziggle/gen/strings.g.dart';
 
 @RoutePage()
 class ConsentPage extends StatelessWidget {
@@ -8,6 +15,69 @@ class ConsentPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: ZiggleAppBar(title: Text(context.t.user.consent.title)),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+          child: _Layout(),
+        ),
+      ),
+    );
+  }
+}
+
+class _Layout extends StatelessWidget {
+  const _Layout();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          context.t.user.consent.description,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+        ),
+        const SizedBox(height: 8),
+        ZigglePressable(
+          onPressed: () => launchUrlString(Strings.termsOfServiceUrl),
+          child: Text(
+            context.t.user.consent.fields.tos,
+            style: const TextStyle(
+              color: Palette.grayText,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        ),
+        ZigglePressable(
+          onPressed: () => launchUrlString(Strings.privacyPolicyUrl),
+          child: Text(
+            context.t.user.consent.fields.privacy,
+            style: const TextStyle(
+              color: Palette.grayText,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        ),
+        Spacer(),
+        Row(
+          children: [
+            Expanded(
+              child: ZiggleButton.cta(
+                outlined: true,
+                child: Text(context.t.user.consent.actions.disagree),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: ZiggleButton.cta(
+                child: Text(context.t.user.consent.actions.agree),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
