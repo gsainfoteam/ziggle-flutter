f17da475e94f1e258ce19f16982408600c09dcf1
dawnfire05 <dawnfire05@naver.com>
2025-09-23 13:24:02 +0900

    fix: rename authorities to permissions

--- lib/app/modules/groups/data/data_sources/remote/group_api.dart
+++ lib/app/modules/groups/data/data_sources/remote/group_api.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:ziggle/app/modules/core/data/dio/groups_dio.dart';
