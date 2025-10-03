a7cc60ae76e2d6b2aa9f8eac7fb5f8d33c0f8d61
akrnwl <moondo28@naver.com>
2025-10-03 15:39:58 +0900

    fix: Update import statement for Hive language adapters

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ import 'package:ziggle/app/values/fonts.dart';
 import 'package:ziggle/app_bloc_observer.dart';
 import 'package:ziggle/firebase_options.dart';
 import 'package:ziggle/gen/strings.g.dart';
-import 'package:ziggle/hive/hive_adapters.dart';
+import 'package:ziggle/hive/hive_language_adapters.dart';
 import 'package:ziggle/hive/hive_registrar.g.dart';
 
 void main() async {
