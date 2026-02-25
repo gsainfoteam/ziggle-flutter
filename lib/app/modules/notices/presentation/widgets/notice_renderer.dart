92f3db990ac6fd01f78d2c69a4f523b49d6a1feb
tkjftkjf <72309529+tkjftkjf@users.noreply.github.com>
2026-02-25 23:54:59 +0900

    feat: add analitics and user check

--- lib/app/modules/notices/presentation/widgets/notice_renderer.dart
+++ lib/app/modules/notices/presentation/widgets/notice_renderer.dart
@@ -308,6 +308,12 @@ class _NoticeRendererState extends State<NoticeRenderer> {
                   ),
                   _ChipButton(
                     onPressed: () async {
+                      AnalyticsRepository.click(
+                        AnalyticsEvent.noticeReport(),
+                      );
+                      if (UserBloc.userOrNull(context) == null) {
+                        return context.showToast(context.t.user.login.description);
+                      }
                       final result = await context.showDialog<bool>(
                         title: context.t.notice.detail.report,
                         content: context.t.notice.detail.reportDescription,
